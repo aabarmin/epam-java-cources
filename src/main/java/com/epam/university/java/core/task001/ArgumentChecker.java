d9f3c04f60d7b2e021273a8ff53fb4772ae3ceb0
ikiselev7 <ikiselev7@gmail.com>
2017-09-02 20:00:32 +0300

    Task 001 & Task 002

---
+++ src/main/java/com/epam/university/java/core/task001/ArgumentChecker.java
@@ -0,0 +1,13 @@
+package com.epam.university.java.core.task001;
+
+/**
+ * Created by ilya on 02.09.17.
+ */
+public class ArgumentChecker implements Checker {
+    @Override
+    public void check(String number) {
+        if(number==null){
+            throw new IllegalArgumentException("one or both of numbers is null");
+        }
+    }
+}
