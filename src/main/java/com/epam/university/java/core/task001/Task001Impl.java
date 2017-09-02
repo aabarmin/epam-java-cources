8657bd65492695a8867bbd440d183ed2011d8c6c
stelmakhmaksim <stelmakh96@mail.ru>
2017-09-02 23:43:26 +0300

    Task001 complete.

--- src/main/java/com/epam/university/java/core/task001/Task001Impl.java
+++ src/main/java/com/epam/university/java/core/task001/Task001Impl.java
@@ -1,4 +1,51 @@
 package com.epam.university.java.core.task001;
 
-public class Task001Impl {
+public class Task001Impl implements Task001 {
+    @Override
+    public double addition(String firstNumber, String secondNumber) {
+        if (firstNumber == null || secondNumber == null) {
+            throw new IllegalArgumentException();
+        }
+        try {
+            return Double.parseDouble(firstNumber) + Double.parseDouble(secondNumber);
+        } catch (NumberFormatException e) {
+            throw new NumberFormatException();
+        }
+    }
+
+    @Override
+    public double subtraction(String firstNumber, String secondNumber) {
+        if (firstNumber == null || secondNumber == null) {
+            throw new IllegalArgumentException();
+        }
+        try {
+            return Double.parseDouble(firstNumber) - Double.parseDouble(secondNumber);
+        } catch (NumberFormatException e) {
+            throw new NumberFormatException();
+        }
+    }
+
+    @Override
+    public double multiplication(String firstNumber, String secondNumber) {
+        if (firstNumber == null || secondNumber == null) {
+            throw new IllegalArgumentException();
+        }
+        try {
+            return Double.parseDouble(firstNumber) * Double.parseDouble(secondNumber);
+        } catch (NumberFormatException e) {
+            throw new NumberFormatException();
+        }
+    }
+
+    @Override
+    public double division(String firstNumber, String secondNumber) {
+        if (firstNumber == null || secondNumber == null) {
+            throw new IllegalArgumentException();
+        }
+        try {
+            return Double.parseDouble(firstNumber) / Double.parseDouble(secondNumber);
+        } catch (NumberFormatException e) {
+            throw new NumberFormatException();
+        }
+    }
 }
