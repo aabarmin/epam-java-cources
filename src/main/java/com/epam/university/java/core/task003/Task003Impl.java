031b1c1968a1206f364236b9689f48a98336d643
Titov Sergei <quosapv@yandex.ru>
2017-09-05 01:50:45 +0300

    added Impl file

---
+++ src/main/java/com/epam/university/java/core/task003/Task003Impl.java
@@ -0,0 +1,38 @@
+package com.epam.university.java.core.task003;
+
+public class Task003Impl implements Task003 {
+    @Override
+    public String[] invert(String[] source) {
+        return new String[0];
+    }
+
+    @Override
+    public String[] join(String[] first, String[] second) {
+        return new String[0];
+    }
+
+    @Override
+    public int findMax(int[] source) {
+        return 0;
+    }
+
+    @Override
+    public String[] filter(String[] source, FilteringCondition condition) {
+        return new String[0];
+    }
+
+    @Override
+    public String[] removeElements(String[] source, String[] toRemote) {
+        return new String[0];
+    }
+
+    @Override
+    public String[] map(String[] source, MappingOperation operation) {
+        return new String[0];
+    }
+
+    @Override
+    public String[] flatMap(String[] source, FlatMappingOperation operation) {
+        return new String[0];
+    }
+}
