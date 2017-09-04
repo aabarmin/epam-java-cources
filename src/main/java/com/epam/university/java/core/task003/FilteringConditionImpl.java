962c85fd4d185fa229f8523033b67154c0c6ec6d
Aleksandr Barmin <barmin.alexander@gmail.com>
2017-09-04 09:04:25 +0300

    Revert "vchuprakov - tasks001-003 done"

--- src/main/java/com/epam/university/java/core/task003/FilteringConditionImpl.java
+++
@@ -1,8 +0,0 @@
-package com.epam.university.java.core.task003;
-
-public final class FilteringConditionImpl implements FilteringCondition {
-    @Override
-    public boolean isValid(final String element) {
-        return !("One".equals(element) || ("Two".equals(element)));
-    }
-}
