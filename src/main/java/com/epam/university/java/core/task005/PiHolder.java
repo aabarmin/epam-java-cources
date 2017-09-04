0e4ae3edeec0f3c1c42702dd2b664c23045ab756
Aleksandr Barmin <aleksandr_barmin@epam.com>
2017-09-04 09:35:27 +0300

    Math task 005

--- src/main/java/com/epam/university/java/core/task005/PiHolder.java
+++ src/main/java/com/epam/university/java/core/task005/PiHolder.java
@@ -1,7 +1,18 @@
 package com.epam.university.java.core.task005;
 
+/**
+ * Interface for data holder.
+ */
 public interface PiHolder {
+    /**
+     * Numerator value.
+     * @return value of numerator
+     */
     int getFirst();
 
+    /**
+     * Denominator value.
+     * @return value of denominator
+     */
     int getSecond();
 }
