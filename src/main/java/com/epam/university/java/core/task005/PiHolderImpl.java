8fde867c4b23d0d316dc6cb4a91e8d5383071eb3
ikiselev7 <ikiselev7@gmail.com>
2017-09-05 17:29:16 +0300

    Task005 done

---
+++ src/main/java/com/epam/university/java/core/task005/PiHolderImpl.java
@@ -0,0 +1,30 @@
+package com.epam.university.java.core.task005;
+
+/**
+ * Created by ilya on 04.09.17.
+ */
+public class PiHolderImpl implements PiHolder {
+    private final int first;
+    private final int second;
+    private final double difference;
+
+    public PiHolderImpl(int first, int second, double difference) {
+        this.first = first;
+        this.second = second;
+        this.difference = difference;
+    }
+
+    @Override
+    public int getFirst() {
+        return first;
+    }
+
+    @Override
+    public int getSecond() {
+        return second;
+    }
+
+    public double getDifference() {
+        return difference;
+    }
+}
