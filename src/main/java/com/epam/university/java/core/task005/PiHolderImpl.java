c2d6affae37aa0011dceb7c09d9834b1586ec480
razyke <waqk@mail.ru>
2017-09-06 13:50:11 +0300

    update

---
+++ src/main/java/com/epam/university/java/core/task005/PiHolderImpl.java
@@ -0,0 +1,13 @@
+package com.epam.university.java.core.task005;
+
+public class PiHolderImpl implements PiHolder {
+    @Override
+    public int getFirst() {
+        return 0;
+    }
+
+    @Override
+    public int getSecond() {
+        return 0;
+    }
+}
