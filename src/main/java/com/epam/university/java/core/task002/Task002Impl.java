c4c080575ac4627b002a0092871ead94dcdb3df2
Astel <astel.develop@gmail.com>
2017-09-07 00:13:59 +0300

    Add Task002Impl

--- src/main/java/com/epam/university/java/core/task002/Task002Impl.java
+++ src/main/java/com/epam/university/java/core/task002/Task002Impl.java
@@ -1,7 +1,131 @@
 package com.epam.university.java.core.task002;
 
+import com.epam.university.java.core.validation.Validator;
+
 /**
  * Created by Александр on 06.09.2017.
  */
-public class Task002Impl {
+public class Task002Impl implements Task002 {
+    private static Validator VALIDATOR =Validator.newInstance(Task002Impl.class);
+
+
+    /**
+     * Check if strings are equals.
+     *
+     * @param firstString  first string instance
+     * @param secondString second string instance
+     * @return if string are equals
+     * @throws IllegalArgumentException if strings not provided
+     */
+    @Override
+    public boolean isEquals(String firstString, String secondString) {
+        VALIDATOR.assertNotNull(firstString);
+        VALIDATOR.assertNotNull(secondString);
+
+        return firstString.equals(secondString);
+    }
+
+    /**
+     * Get most left @{number} of symbols.
+     *
+     * @param sourceString source string
+     * @param number       amount of symbols to return
+     * @return new string
+     * @throws IllegalArgumentException if string not provided or number less then zero
+     */
+    @Override
+    public String left(String sourceString, int number) {
+        VALIDATOR.assertNotNull(sourceString);
+        VALIDATOR.assertMoreZero(number);
+
+        if ( number > sourceString.length())
+            return sourceString;
+        else
+            return sourceString.substring(0,number);
+    }
+
+    /**
+     * Get most right @{number} of symbols.
+     *
+     * @param sourceString source string
+     * @param number       amount of symbols to return
+     * @return new string
+     * @throws IllegalArgumentException if string not provided or number less then zero
+     */
+    @Override
+    public String right(String sourceString, int number) {
+        VALIDATOR.assertNotNull(sourceString);
+        VALIDATOR.assertMoreZero(number);
+
+        if ( number > sourceString.length())
+            return sourceString;
+        else
+            return sourceString.substring(sourceString.length()-number,sourceString.length());
+    }
+
+    /**
+     * Get the characters before separator.
+     *
+     * @param sourceString source string
+     * @param separator    separator
+     * @return characters before separator
+     * @throws IllegalArgumentException if strings not provided
+     */
+    @Override
+    public String left(String sourceString, String separator) {
+        VALIDATOR.assertNotNull(sourceString);
+        VALIDATOR.assertNotNull(separator);
+
+        int endIndex = sourceString.indexOf(separator);
+        return sourceString.substring(0,endIndex);
+    }
+
+    /**
+     * Get the characters after separator.
+     *
+     * @param sourceString source string
+     * @param separator    separator
+     * @return characters after separator
+     * @throws IllegalArgumentException if strings not provided
+     */
+    @Override
+    public String right(String sourceString, String separator) {
+        VALIDATOR.assertNotNull(sourceString);
+        VALIDATOR.assertNotNull(separator);
+
+        int startIndex = sourceString.indexOf(separator);
+        return sourceString.substring(startIndex+separator.length(),sourceString.length());
+    }
+
+    /**
+     * Split source string by split character.
+     *
+     * @param sourceString source string
+     * @param split        character to split for
+     * @return array of parts
+     * @throws IllegalArgumentException if strings not provided
+     */
+    @Override
+    public String[] split(String sourceString, String split) {
+        VALIDATOR.assertNotNull(sourceString);
+        VALIDATOR.assertNotNull(split);
+
+        return sourceString.split(split);
+    }
+
+    /**
+     * Connect array of strings with glue
+     *
+     * @param sourceCollection collection of strings
+     * @param glue             glue character
+     * @return result string
+     * @throws IllegalArgumentException if strings not provided
+     */
+    @Override
+    public String join(String[] sourceCollection, String glue) {
+        VALIDATOR.assertNotNull(sourceCollection);
+        VALIDATOR.assertNotNull(glue);
+
+        return String.join(glue,sourceCollection);
+    }
 }
