17d0135b34bb5bd5b22314b701b6b79d284683c9
razyke <waqk@mail.ru>
2017-09-06 12:37:26 +0300

    Task 2,3,4 complete.

---
+++ src/main/java/com/epam/university/java/core/task003/FlatMappingOperationImpl.java
@@ -0,0 +1,16 @@
+package com.epam.university.java.core.task003;
+
+
+public class FlatMappingOperationImpl implements FlatMappingOperation {
+    @Override
+    public String[] flatMap(String source) {
+        if (source==null){
+            throw new IllegalArgumentException();
+        }
+        String[] array = source.split(",");
+        for (int i = 0; i < array.length; i++) {
+            array[i] = array[i].trim();
+        }
+        return array;
+    }
+}
---
+++ src/main/java/com/epam/university/java/core/task004/Task004Impl.java
@@ -0,0 +1,64 @@
+package com.epam.university.java.core.task004;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Task004Impl implements Task004 {
+    @Override
+    public String[] intersection(String[] first, String[] second) {
+        checkArrays(first,second);
+        ArrayList<String> container = new ArrayList<>();
+        for (int i = 0; i < first.length; i++) {
+            for (int j = 0; j < second.length; j++) {
+                if (first[i].equals(second[j])){
+                    if (!container.contains(second[j]))
+                        container.add(second[j]);
+                }
+            }
+        }
+        String[] result = new String[container.size()];
+        result = container.toArray(result);
+
+        return result;
+    }
+
+    @Override
+    public String[] union(String[] first, String[] second) {
+        checkArrays(first,second);
+        ArrayList<String> container = new ArrayList<>();
+
+        for (int i = 0; i < first.length; i++) {
+            if (!container.contains(first[i])){
+                container.add(first[i]);
+            }
+        }
+
+
+        for (int i = 0; i < first.length; i++) {
+            for (int j = 0; j < second.length; j++) {
+                if (!first[i].equals(second[j])){
+                    if (!container.contains(second[j]))
+                        container.add(second[j]);
+                }
+            }
+        }
+
+       /* for (int i = 0; i < container.size(); i++) {
+            for (int j = 0; j < container.size(); j++) {
+                if (container.get(i).equals(container.get(j))){
+                    container.remove(container.get(j));
+                }
+            }
+        }*/
+        String[] result = new String[container.size()];
+        result = container.toArray(result);
+
+        return result;
+    }
+
+    private void checkArrays(String[] first, String[] second){
+        if (first==null||second==null){
+            throw new IllegalArgumentException();
+        }
+    }
+}
