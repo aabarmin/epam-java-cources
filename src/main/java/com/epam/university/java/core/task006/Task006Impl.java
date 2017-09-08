dfb3b83403542290a41ee7fa5cc3010d5df6c4cf
victorxup <victor.chuprakov@gmail.com>
2017-09-08 17:25:20 +0300

    Tasks 006-007 done

---
+++ src/main/java/com/epam/university/java/core/task006/Task006Impl.java
@@ -0,0 +1,47 @@
+package com.epam.university.java.core.task006;
+
+import java.util.Collection;
+
+/**
+ * This task provide collection of measurements of voltage and amperage in resistance.
+ * Using Least Square method determine value of resistance.
+ * Task006 implementation.
+ */
+public class Task006Impl implements Task006 {
+
+    /**
+     * * Message for the case measurements not provided.
+     */
+    private static final String MSG_NO_ARGS = "measurements not provided";
+
+    @Override
+    public double resistance(Collection<Measurement> measurements) {
+        if (measurements == null) throw new IllegalArgumentException(MSG_NO_ARGS);
+
+        double sumVoltage = 0.0;
+        double sumAmperage = 0.0;
+        for (Measurement m : measurements) {
+            sumVoltage += m.getVoltage();
+            sumAmperage += m.getAmperage();
+
+        }
+        double averageVoltage = sumVoltage / measurements.size();
+        double averageAmperage = sumAmperage / measurements.size();
+
+        double denominatorOfResistance = 0.0;
+        double numeratorOfResistance = 0.0;
+        for (Measurement m : measurements) {
+            double v = m.getAmperage() - averageAmperage;
+            denominatorOfResistance += v * v;
+            numeratorOfResistance += (m.getAmperage() - averageAmperage) * (m.getVoltage() - averageVoltage);
+        }
+
+        double result = 0.0;
+        if (denominatorOfResistance != 0) {
+            double resistance = numeratorOfResistance / denominatorOfResistance;
+            result = Math.round(resistance * 1000) / 1000.0;
+        }
+        return result;
+    }
+}
+
