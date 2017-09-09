a21060cf99c79a65feaf742ec766dfa826d93590
ikiselev7 <ikiselev7@gmail.com>
2017-09-09 17:57:37 +0300

    Task 007 changed

--- src/main/java/com/epam/university/java/core/task007/Task007Impl.java
+++ src/main/java/com/epam/university/java/core/task007/Task007Impl.java
@@ -23,16 +23,7 @@ public class Task007Impl implements Task007 {
         IntStream.range(0,first.size())
                 .map(index -> {
                     Integer e = integersFirst[index];
-                    IntStream.range(index, index + integersSecond.length)
-                            .map(ind -> {
-                                Integer i = integersSecond[ind- index];
-                                if(result.containsKey(ind)){
-                                    result.put(ind, result.get(ind).intValue()+i* e);
-                                }else{
-                                    result.put(ind,i* e);
-                                }
-                                return ind;
-                            }).count();
+                    operationWithSecond(result, integersSecond, index, e);
                     return index;
                 }).boxed().count();
 
@@ -42,6 +33,19 @@ public class Task007Impl implements Task007 {
         resList.add(0);
 
         return resList;
+    }
+
+    private void operationWithSecond(Map<Integer, Integer> result, Integer[] integersSecond, int index, Integer e) {
+        IntStream.range(index, index + integersSecond.length)
+                .map(ind -> {
+                    Integer i = integersSecond[ind- index];
+                    if(result.containsKey(ind)){
+                        result.put(ind, result.get(ind).intValue()+i* e);
+                    }else{
+                        result.put(ind,i* e);
+                    }
+                    return ind;
+                }).count();
     }
 
 }
