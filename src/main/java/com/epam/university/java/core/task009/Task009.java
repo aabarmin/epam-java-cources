88dcaee14a4930da495ea18a79df9b45b1cab2ad
ikiselev7 <ikiselev7@gmail.com>
2017-09-09 21:03:04 +0300

    Travis checking

--- src/main/java/com/epam/university/java/core/task009/Task009.java
+++ src/main/java/com/epam/university/java/core/task009/Task009.java
@@ -1,11 +1,7 @@
 package com.epam.university.java.core.task009;
 
 import java.io.File;
-<<<<<<< HEAD
-import java.io.FileNotFoundException;
 import java.io.IOException;
-=======
->>>>>>> cc23fbcfc6c655261c707eb29b8427ea48f7f05b
 import java.util.Collection;
 
 /**
@@ -13,12 +9,6 @@ import java.util.Collection;
  */
 public interface Task009 {
     /**
-<<<<<<< HEAD
-     * Source file contains words which separated with spaces. You need to output all different words. Same
-     * word in upper and lower case should be counted as equal.
-     *
-     * Tip: you can use Set for it.
-=======
      * Source file contains words which separated with spaces.
      * <p>
      *     You need to output all different words.
@@ -27,14 +17,11 @@ public interface Task009 {
      * <p>
      *     Tip: you can use Set for it.
      * </p>
->>>>>>> cc23fbcfc6c655261c707eb29b8427ea48f7f05b
      *
      * @param sourceFile source file
      * @return collection of different words
      */
-<<<<<<< HEAD
+
     Collection<String> countWords(File sourceFile) throws IOException;
-=======
-    Collection<String> countWords(File sourceFile) throws IOException;
->>>>>>> cc23fbcfc6c655261c707eb29b8427ea48f7f05b
+
 }
