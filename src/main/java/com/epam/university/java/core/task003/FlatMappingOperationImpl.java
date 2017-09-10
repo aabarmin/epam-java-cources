cab736653292f889a4c110e28918cd3cfbe4c38b
a-sakhnenko <aksakhnenko@gmail.com>
2017-09-10 22:58:28 +0300

    Fixed Tasks 1-3

--- src/main/java/com/epam/university/java/core/task003/FlatMappingOperationImpl.java
+++ src/main/java/com/epam/university/java/core/task003/FlatMappingOperationImpl.java
@@ -3,36 +3,36 @@ package com.epam.university.java.core.task003;
 public class FlatMappingOperationImpl implements FlatMappingOperation {
     @Override
     public String[] flatMap(String source) {
-        int sizeOfOutput = 0;
-        String outputStr = "";
-        for (int i = 0; i < source.length(); i++) {
-            if (source.charAt(i) >= '0' && source.charAt(i) <= '9') {
-                sizeOfOutput++;
-                do {
-                    outputStr += source.charAt(i);
-                    i++;
-                } while (i < source.length() && source.charAt(i) >= '0' && source.charAt(i) <= '9');
-                outputStr += " ";
+
+        String[] preOutput = source.split("[ ]*,[ ]*");
+
+        return DeletionOfDuplicates.deleteDuplicates(preOutput);
+    }
+}
+
+class DeletionOfDuplicates {
+    static String[] deleteDuplicates (String[] source) {
+        boolean[] isDuplicate = new boolean[source.length];
+        int sizeOfOutput = source.length;
+        for (int i = 0; i < source.length; i++) {
+            isDuplicate[i] = false;
+            for (int j = 0; j < i; j++) {
+                if (source[j].equals(source[i])) {
+                    isDuplicate[i] = true;
+                    sizeOfOutput--;
+                    break;
+                }
             }
         }
-        String[] preOutput = new String[sizeOfOutput];
+        String[] dest = new String[sizeOfOutput];
+        int i = 0;
         int j = 0;
-        for (int i = 0; i < sizeOfOutput; i++) {
-            preOutput[i] = "";
-            while (j < outputStr.length() && outputStr.charAt(j) != ' ') {
-                preOutput[i] += outputStr.charAt(j++);
+        while (i < source.length && j < sizeOfOutput) {
+            if (!isDuplicate[i]) {
+                dest[j++] = source[i];
             }
-            j++;
+            i++;
         }
-        String[] output = new String[0];
-        String[] temp;
-        Task003Impl assistant = new Task003Impl();
-        for (int i = 0; i < preOutput.length; i++) {
-            temp = new String[] {""};
-            temp[0] = preOutput[i];
-            temp = assistant.removeElements(temp, output);
-            output = assistant.join(output, temp);
-        }
-        return output;
+        return dest;
     }
 }
