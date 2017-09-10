184d166adec55af6b638604779aa61506d55a484
stelmakhmaksim <stelmakh96@mail.ru>
2017-09-10 23:24:11 +0300

    Task 3 is done, all tests are passed!

--- src/main/java/com/epam/university/java/core/task003/Task003Impl.java
+++ src/main/java/com/epam/university/java/core/task003/Task003Impl.java
@@ -1,52 +1,193 @@
 package com.epam.university.java.core.task003;
 
+import java.util.Arrays;
+
 public class Task003Impl implements Task003 {
     @Override
     public String[] invert(String[] source) {
         if (source == null) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("Array not provided!");
         }
-        for (int i = 0, j = source.length - 1; i < source.length / 2 && j > (source.length - 1) / 2; i++, j--) {
-            String str = source[i];
-            source[i] = source[j];
-            source[j] = str;
+        return reverseArr(source);
+    }
+
+    public static <T> T[] reverseArr(T[] values) {
+        if (values == null) {
+            throw new IllegalArgumentException("Array not provided!");
         }
-
-
-        /*String[] arr = new String[source.length];
-        for (int i = source.length - 1, j = 0; i >= 0 && j < arr.length; i--, j++) {
-            arr[j] = source[i];
-        }*/
-        return source;
+        for (int i = 0, j = values.length - 1; i < values.length / 2 && j > (values.length - 1) / 2; i++, j--) {
+            T str = values[i];
+            values[i] = values[j];
+            values[j] = str;
+        }
+        return values;
     }
 
     @Override
     public String[] join(String[] first, String[] second) {
-        return new String[0];
+        if (first == null || second == null) {
+            throw new IllegalArgumentException("Arrays not provided!");
+        }
+        String[] third = new String[first.length + second.length];
+        for (int i = 0, j = 0, k = 0; i < third.length; i++) {
+            if (i < first.length) {
+                third[i] = first[j++];
+            } else {
+                third[i] = second[k++];
+            }
+        }
+        return third;
     }
 
     @Override
     public int findMax(int[] source) {
-        return 0;
+        if (source == null) {
+            throw new IllegalArgumentException("Array not provided!");
+        }
+        int max = source[0];
+        for (int i : source) {
+            if (i > max)
+                max = i;
+        }
+        return max;
     }
 
     @Override
     public String[] filter(String[] source, FilteringCondition condition) {
-        return new String[0];
+        if (source == null || condition == null) {
+            throw new IllegalArgumentException("Array or condition not provided!");
+        }
+        int countElement = 0;
+        boolean mask[] = new boolean[source.length];
+        for (int i = 0; i < source.length; i++) {
+            if (condition.isValid(source[i])) {
+                mask[i] = true;
+                countElement++;
+            }
+        }
+
+        String[] filterArr = new String[countElement];
+        for (int i = 0, j = 0; i < source.length && j < filterArr.length; i++) {
+            if (mask[i]) {
+                filterArr[j++] = source[i];
+            }
+        }
+
+        return filterArr;
     }
 
     @Override
     public String[] removeElements(String[] source, String[] toRemote) {
-        return new String[0];
+        if (source == null || toRemote == null) {
+            throw new IllegalArgumentException("Arrays not provided!");
+        }
+        String[] finalString = new String[source.length];
+
+        for (String str : toRemote) {
+            for (int j = 0, k = 0; k < source.length && j < finalString.length; k++) {
+                if (!str.equals(source[k]) && str != null) {
+                    finalString[j++] = source[k];
+                }
+            }
+            int count = 0;
+            for (String s : finalString) {
+                if (s != null) {
+                    count++;
+                }
+            }
+
+            source = finalString;
+            if (finalString.length != count) {
+                finalString = new String[count];
+            }
+        }
+
+        for (int n = 0, q = 0; n < source.length; n++) {
+            if (source[n] != null) {
+                finalString[q++] = source[n];
+            }
+        }
+        return finalString;
     }
 
     @Override
     public String[] map(String[] source, MappingOperation operation) {
-        return new String[0];
+        if (source == null || operation == null) {
+            throw new IllegalArgumentException("Parameters not provided!");
+        }
+        for (int i = 0; i < source.length; i++) {
+            source[i] = operation.map(source[i]);
+        }
+        return source;
     }
 
     @Override
     public String[] flatMap(String[] source, FlatMappingOperation operation) {
-        return new String[0];
+        if (source == null || operation == null) {
+            throw new IllegalArgumentException("Parameters not provided!");
+        }
+
+        String[][] strArrArr = new String[source.length][];
+        for (int i = 0; i < source.length; i++) {
+            strArrArr[i] = operation.flatMap(source[i]);
+        }
+
+        int count = 0;
+        for (String[] strings : strArrArr) {
+            count += strings.length;
+        }
+
+        String[] strArr = new String[count];
+        for (int i = 0, k = 0; i < strArrArr.length; i++) {
+            for (int j = 0; j < strArrArr[i].length; j++) {
+                strArr[k++] = strArrArr[i][j];
+            }
+        }
+
+        Integer[] numArr = new Integer[strArr.length];
+        try {
+            for (int i = 0; i < numArr.length; i++) {
+                numArr[i] = Integer.parseInt(strArr[i]);
+            }
+        } catch (NumberFormatException e) {
+            numArr = null;
+        }
+
+        if (numArr != null) {
+            Arrays.sort(numArr);
+            return reverseArr(removeDuplicates(numArr));
+        } else {
+            Arrays.sort(strArr);
+            return reverseArr(removeDuplicates(strArr));
+        }
+    }
+
+    public static <T> String[] removeDuplicates(T[] values) {
+        boolean mask[] = new boolean[values.length];
+        int removeCount = 0;
+
+        for (int i = 0; i < values.length; i++) {
+            if (!mask[i]) {
+                for (int j = i + 1; j < values.length; j++) {
+                    if (values[i].equals(values[j])) {
+                        mask[j] = true;
+                        removeCount++;
+                    }
+                }
+            }
+        }
+
+        String[] result = new String[values.length - removeCount];
+
+        for (
+                int i = 0, j = 0;
+                i < values.length; i++)
+
+        {
+            if (!mask[i]) {
+                result[j++] = String.valueOf(values[i]);
+            }
+        }
+        return result;
     }
 }
