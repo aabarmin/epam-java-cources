2d6444895d4bb2b8782e6a3917bff2672686cb61
stelmakhmaksim <stelmakh96@mail.ru>
2017-09-10 23:24:11 +0300

    Task 3 is done, all tests are passed!

--- src/main/java/com/epam/university/java/core/task003/MappingOperationImpl.java
+++ src/main/java/com/epam/university/java/core/task003/MappingOperationImpl.java
@@ -3,6 +3,22 @@ package com.epam.university.java.core.task003;
 public class MappingOperationImpl implements MappingOperation {
     @Override
     public String map(String source) {
-        return null;
+        if (source == null) {
+            throw new IllegalArgumentException("String not provided!");
+        }
+
+        char[] ch = source.toCharArray();
+        Character[] characters = new Character[ch.length];
+        for (int i = 0; i < characters.length; i++) {
+            characters[i] = ch[i];
+        }
+
+        characters = Task003Impl.reverseArr(characters);
+
+        for (int i = 0; i < ch.length; i++) {
+            ch[i] = characters[i];
+        }
+
+        return new String(ch);
     }
 }
