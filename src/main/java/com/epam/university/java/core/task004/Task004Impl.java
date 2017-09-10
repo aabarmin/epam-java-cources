848126fcec453019eaa8fb784a4c021948527ba3
a-sakhnenko <aksakhnenko@gmail.com>
2017-09-10 21:26:31 +0300

    Task004

--- src/main/java/com/epam/university/java/core/task004/Task004Impl.java
+++ src/main/java/com/epam/university/java/core/task004/Task004Impl.java
@@ -8,22 +8,39 @@ public class Task004Impl implements Task004 {
         }
         String[] tempArray = new String[Math.min(first.length, second.length)];
         int sizeOfOutputArray = 0;
-        for (int i = 0; i < first.length; i++) {
-            for (int j = 0; j < second.length; j++) {
-                if (first[i].equals(second[j])) {
-                    tempArray[sizeOfOutputArray++] = first[i];
+        for (String aFirst : first) {
+            for (String aSecond : second) {
+                if (aFirst.equals(aSecond)) {
+                    tempArray[sizeOfOutputArray++] = aFirst;
                 }
             }
         }
         String[] outputArray = new String[sizeOfOutputArray];
-        for (int i = 0; i < sizeOfOutputArray; i++) {
-            outputArray[i] = tempArray[i];
-        }
+        System.arraycopy(tempArray, 0, outputArray, 0, sizeOfOutputArray);
         return outputArray;
     }
 
     @Override
     public String[] union(String[] first, String[] second) {
-        return new String[0];
+        if (first == null || second == null) {
+            throw new IllegalArgumentException();
+        }
+        String[] tempArray = new String[first.length + second.length];
+        int indexOfOutputArray = first.length;
+        System.arraycopy(first, 0, tempArray, 0, first.length);
+        boolean isUnique;
+        for (String aSecond : second) {
+            isUnique = true;
+            for (String aFirst : first) {
+                if (aFirst.equals(aSecond)) {
+                    isUnique = false;
+                    break;
+                }
+            }
+            if (isUnique) tempArray[indexOfOutputArray++] = aSecond;
+        }
+        String[] outputArray = new String[indexOfOutputArray];
+        System.arraycopy(tempArray, 0, outputArray, 0, indexOfOutputArray);
+        return outputArray;
     }
 }
