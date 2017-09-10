82c60332dcf91a82b7f228cc3b021fe69fc4fc96
stelmakhmaksim <stelmakh96@mail.ru>
2017-09-10 23:24:11 +0300

    Task 3 is done, all tests are passed!

--- src/main/java/com/epam/university/java/core/task003/FlatMappingOperationImpl.java
+++ src/main/java/com/epam/university/java/core/task003/FlatMappingOperationImpl.java
@@ -1,8 +1,15 @@
 package com.epam.university.java.core.task003;
 
-public class FlatMappingOperationImpl implements FlatMappingOperation{
+public class FlatMappingOperationImpl implements FlatMappingOperation {
     @Override
     public String[] flatMap(String source) {
-        return new String[0];
+        if (source == null) {
+            return new String[]{};
+        }
+        String[] arr = source.split(",");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = arr[i].trim();
+        }
+        return arr;
     }
 }
