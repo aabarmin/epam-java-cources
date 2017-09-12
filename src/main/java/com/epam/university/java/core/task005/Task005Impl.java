830a19a94eac7c1aec61bb18b97d9ddafef91e40
stelmakhmaksim <stelmakh96@mail.ru>
2017-09-12 16:15:37 +0300

    Code corrected for style guide.

--- src/main/java/com/epam/university/java/core/task005/Task005Impl.java
+++ src/main/java/com/epam/university/java/core/task005/Task005Impl.java
@@ -6,7 +6,8 @@ public class Task005Impl implements Task005 {
     @Override
     public PiHolder findPi(int digits) {
         if (digits <= 0 || digits > 10) {
-            throw new IllegalArgumentException("Digits less or equals to the zero, or more than ten!");
+            throw new IllegalArgumentException("Digits less or equals to the zero,"
+                    + " or more than ten!");
         }
         PiHolder numbers = new PiHolderImpl();
         return numbers;
