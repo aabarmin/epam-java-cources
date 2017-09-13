36ee1d9def062d20a0b57848142263d6586ec0a0
stelmakhmaksim <stelmakh96@mail.ru>
2017-09-13 16:34:35 +0300

    Task 5 is done. All tests are passed.

--- src/main/java/com/epam/university/java/core/task005/PiHolderImpl.java
+++ src/main/java/com/epam/university/java/core/task005/PiHolderImpl.java
@@ -1,18 +1,21 @@
 package com.epam.university.java.core.task005;
 
 public class PiHolderImpl implements PiHolder {
-    private int numerator = 3;
-    private int denomirator = 1;
+    private int numerator;
+    private int denominator;
+
+    PiHolderImpl(int numerator, int denominator) {
+        this.numerator = numerator;
+        this.denominator = denominator;
+    }
 
     @Override
     public int getFirst() {
-
         return numerator;
     }
 
     @Override
     public int getSecond() {
-
-        return denomirator;
+        return denominator;
     }
 }
--- src/main/java/com/epam/university/java/core/task005/Task005Impl.java
+++ src/main/java/com/epam/university/java/core/task005/Task005Impl.java
@@ -1,15 +1,36 @@
 package com.epam.university.java.core.task005;
 
-public class Task005Impl implements Task005 {
+import static java.lang.Math.pow;
+import static java.lang.Math.PI;
+import static java.lang.Math.round;
 
+public class Task005Impl implements Task005 {
 
     @Override
     public PiHolder findPi(int digits) {
-        if (digits <= 0 || digits > 10) {
+        if (digits <= 0 || digits > 9) {
             throw new IllegalArgumentException("Digits less or equals to the zero,"
                     + " or more than ten!");
         }
-        PiHolder numbers = new PiHolderImpl();
+
+        PiHolder numbers = null;
+        double currentMinDiff = PI;
+        //beginning of the numerator range
+        int currentNum = (int) (round((pow(10, (digits - 1))) * PI));
+        //beginning of the denominator range
+        int currentDen = (int) (round(pow(10, (digits - 1))));
+        //maximum value of retrieved numbers
+        int maxVal = (int) (round((pow(10, digits) - 1)));
+
+        while (currentNum < maxVal) {
+            double currentDiff = Math.abs((double) currentNum / currentDen - PI);
+            if (currentDiff < currentMinDiff) {
+                currentMinDiff = currentDiff;
+                numbers = new PiHolderImpl(currentNum, currentDen);
+            }
+            currentNum++;
+            currentDen = (int) (round(currentNum / PI));
+        }
         return numbers;
     }
 }
