c42d4fc5d4378514abffdddd4f2af4fc47f572c0
ikiselev7 <ikiselev7@gmail.com>
2017-09-14 20:55:40 +0300

    some changes

---
+++ src/main/java/com/epam/university/java/core/task012/GraphImpl.java
@@ -0,0 +1,22 @@
+package com.epam.university.java.core.task012;
+
+/**
+ * Created by ilya on 13.09.17.
+ */
+public class GraphImpl implements Graph {
+
+    @Override
+    public void createEdge(int from, int to) {
+
+    }
+
+    @Override
+    public boolean edgeExists(int from, int to) {
+        return false;
+    }
+
+    @Override
+    public void removeEdge(int from, int to) {
+
+    }
+}
