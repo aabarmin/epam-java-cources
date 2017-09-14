6782e9c17777a1ea9c5b68c7f93c3c6c2e7022e4
Astel <astel.develop@gmail.com>
2017-09-14 03:49:00 +0300

    Task006 complited

--- src/main/java/com/epam/university/java/core/task006/Task006Impl.java
+++ src/main/java/com/epam/university/java/core/task006/Task006Impl.java
@@ -5,9 +5,6 @@ import com.epam.university.java.core.validation.Validator;
 import java.util.Collection;
 import java.util.stream.StreamSupport;
 
-/**
- * Created by Александр on 06.09.2017.
- */
 public class Task006Impl implements Task006 {
     private static Validator VALIDATOR = Validator.newInstance(Task006Impl.class);
 
@@ -23,9 +20,32 @@ public class Task006Impl implements Task006 {
     public double resistance(Collection<Measurement> measurements) {
         VALIDATOR.assertNotNull(measurements);
 
-        //Double num = 0;
-        //measurements.stream().forEach(m -> num += (m.getAmperage()*m.getVoltage()));
+        if (measurements.isEmpty()) {
+            return 0.0;
+        }
 
-        return 0;
+        double avgVoltage = 0;
+        double avgAmperage = 0;
+        int size = measurements.size();
+
+        for (Measurement iter : measurements) {
+            avgAmperage += iter.getAmperage();
+            avgVoltage += iter.getVoltage();
+        }
+        avgAmperage /= size;
+        avgVoltage /= size;
+
+        double denominator = 0;
+        double numerator = 0;
+        for (Measurement iter : measurements) {
+            double deviationAmperage = iter.getAmperage() - avgAmperage;
+            double deviationVoltage = iter.getVoltage() - avgVoltage;
+            numerator += deviationAmperage * deviationVoltage;
+
+            denominator += Math.pow(iter.getAmperage() - avgAmperage, 2);
+        }
+
+        double resistance = numerator / denominator;
+        return Math.round(resistance * 1000) / 1000.0;
     }
 }
