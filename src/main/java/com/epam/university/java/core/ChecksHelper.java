6ab2aa8743c26c4af59198419ab7d4b0abe8e8ca
Titov Sergei <quosapv@yandex.ru>
2017-09-16 02:46:01 +0300

    Travis java-doc saticfaction for ChecksHelper

--- src/main/java/com/epam/university/java/core/ChecksHelper.java
+++ src/main/java/com/epam/university/java/core/ChecksHelper.java
@@ -1,8 +1,7 @@
 package com.epam.university.java.core;
 
 /**
- * class is to be used by all the TaskXXX classes
- * for params consistency checking.
+ * class is to be used by all the TaskXXX classes for params consistency checking.
  *
  * @author  Sergei Titov
  */
@@ -13,7 +12,7 @@ public class ChecksHelper {
      * serves as exception-thrower in case of too bad parameter.
      *
      * @param arg is to be checked for not being a null
-     * @throws IllegalArgumentException
+     * @throws IllegalArgumentException otherwise
      */
     public static void checkForNull(Object arg) throws IllegalArgumentException {
         if (null == arg) {
@@ -25,7 +24,7 @@ public class ChecksHelper {
     * serves as exception-thrower in case of too bad parameter.
     *
     * @param arg is to be checked for not being a null or an empty array
-    * @throws IllegalArgumentException
+    * @throws IllegalArgumentException otherwise
     */
     public static void checkForEmptiness(int[] arg) throws IllegalArgumentException {
         if ((null == arg) || (0 == arg.length)) {
@@ -38,10 +37,12 @@ public class ChecksHelper {
     *
     * @param firstNumber is to be checked for not being a null
     * @param secondNumber is to be checked for not being a null
-    * @throws IllegalArgumentException
+    * @throws IllegalArgumentException otherwise
     */
-    public static void checkForNullBothArguments(Object firstNumber,
-                                                 Object secondNumber) throws IllegalArgumentException {
+    public static void checkForNullBothArguments(
+            Object firstNumber,
+            Object secondNumber) throws IllegalArgumentException
+    {
         if (null == firstNumber && null == secondNumber) {
             throw new IllegalArgumentException();
         }
@@ -52,10 +53,12 @@ public class ChecksHelper {
      *
      * @param firstNumber is to be checked for not being only a single whitespace-char string
      * @param secondNumber is to be checked for not being only a single whitespace-char string
-     * @throws IllegalArgumentException
+     * @throws IllegalArgumentException otherwise
      */
-    public static void checkForEmptyBothArguments(String firstNumber,
-                                                  String secondNumber) throws IllegalArgumentException {
+    public static void checkForEmptyBothArguments(
+            String firstNumber,
+            String secondNumber) throws IllegalArgumentException
+    {
         if (firstNumber.equals(" ") && secondNumber.equals(" ")) {
             throw new IllegalArgumentException();
         }
