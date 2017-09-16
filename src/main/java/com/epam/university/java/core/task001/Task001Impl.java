3add545c97ebac49d30b555b19a96f247f764b35
Titov Sergei <quosapv@yandex.ru>
2017-09-16 11:28:09 +0300

    Javadoc cosmetics Task001Impl

--- src/main/java/com/epam/university/java/core/task001/Task001Impl.java
+++ src/main/java/com/epam/university/java/core/task001/Task001Impl.java
@@ -2,36 +2,70 @@ package com.epam.university.java.core.task001;
 
 import com.epam.university.java.core.ChecksHelper;
 
+/**
+ * implementation class for Task001.
+ *
+ * @author Sergei Titov
+ */
 public class Task001Impl implements Task001 {
+
+    /**
+     * add two params.
+     *
+     * @param firstNumber string value of first number
+     * @param secondNumber string value of second number
+     * @return the sum
+     */
     @Override
     public double addition(String firstNumber, String secondNumber) {
 
-        ChecksHelper.checkForNullBothArguments( firstNumber, secondNumber );
-        ChecksHelper.checkForEmptyBothArguments( firstNumber, secondNumber );
-        return Double.parseDouble( firstNumber ) + Double.parseDouble( secondNumber );
+        ChecksHelper.checkForNullBothArguments(firstNumber, secondNumber);
+        ChecksHelper.checkForEmptyBothArguments(firstNumber, secondNumber);
+        return Double.parseDouble(firstNumber) + Double.parseDouble(secondNumber);
     }
 
+    /**
+     * subtracts two params.
+     *
+     * @param firstNumber string value of first number
+     * @param secondNumber string value of second number
+     * @return the difference
+     */
     @Override
     public double subtraction(String firstNumber, String secondNumber) {
 
-        ChecksHelper.checkForNullBothArguments( firstNumber, secondNumber );
-        ChecksHelper.checkForEmptyBothArguments( firstNumber, secondNumber );
-        return Double.parseDouble( firstNumber ) - Double.parseDouble( secondNumber );
+        ChecksHelper.checkForNullBothArguments(firstNumber, secondNumber);
+        ChecksHelper.checkForEmptyBothArguments(firstNumber, secondNumber);
+        return Double.parseDouble(firstNumber) - Double.parseDouble(secondNumber);
     }
 
+    /**
+     * multiplies two params.
+     *
+     * @param firstNumber string value of first number
+     * @param secondNumber string value of second number
+     * @return
+     */
     @Override
     public double multiplication(String firstNumber, String secondNumber) {
 
-        ChecksHelper.checkForNullBothArguments( firstNumber, secondNumber );
-        ChecksHelper.checkForEmptyBothArguments( firstNumber, secondNumber );
-        return Double.parseDouble( firstNumber ) * Double.parseDouble( secondNumber );
+        ChecksHelper.checkForNullBothArguments(firstNumber, secondNumber);
+        ChecksHelper.checkForEmptyBothArguments(firstNumber, secondNumber);
+        return Double.parseDouble(firstNumber) * Double.parseDouble(secondNumber);
     }
 
+    /**
+     * divides the first param by the second.
+     * 
+     * @param firstNumber string value of first number
+     * @param secondNumber string value of second number
+     * @return
+     */
     @Override
     public double division(String firstNumber, String secondNumber) {
 
-        ChecksHelper.checkForNullBothArguments( firstNumber, secondNumber );
-        ChecksHelper.checkForEmptyBothArguments( firstNumber, secondNumber );
-        return Double.parseDouble( firstNumber ) / Double.parseDouble( secondNumber );
+        ChecksHelper.checkForNullBothArguments(firstNumber, secondNumber);
+        ChecksHelper.checkForEmptyBothArguments(firstNumber, secondNumber);
+        return Double.parseDouble(firstNumber) / Double.parseDouble(secondNumber);
     }
 }
--- src/main/java/com/epam/university/java/core/task003/FlatMappingOperationImpl.java
+++ src/main/java/com/epam/university/java/core/task003/FlatMappingOperationImpl.java
@@ -1,13 +1,18 @@
 package com.epam.university.java.core.task003;
 
+/**
+ *  implementation class.
+ *
+ *
+ */
 public class FlatMappingOperationImpl implements FlatMappingOperation {
 
     @Override
     public String[] flatMap(String source) {
 
-        if( null == source )
+        if (null == source) {
             return null;
-
+        }
         return source.split(",");
     }
 }
