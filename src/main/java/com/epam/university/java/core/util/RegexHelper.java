8ef5d9ed3d1692cc4552ead96e9c6fd5ce4fe6a4
victorxup <victor.chuprakov@gmail.com>
2017-09-16 19:33:45 +0300

    util/RegexHelper.java added

---
+++ src/main/java/com/epam/university/java/core/util/RegexHelper.java
@@ -0,0 +1,16 @@
+package com.epam.university.java.core.util;
+
+/**
+ * Just a class for some help with regular expressions.
+ */
+public class RegexHelper {
+    /**
+     * Regular expression that filters chars, that cannot
+     * be a part of the word.
+     */
+    public static final String REGEX_NON_WORDS = "[^A-Za-z0-9\\-]";
+
+}
+
+
+
