615b97d8a040c79bbb7d4df12be651c9d1236efc
a-sakhnenko <aksakhnenko@gmail.com>
2017-09-17 23:18:40 +0300

    Task009 is done

---
+++ src/main/java/com/epam/university/java/core/task009/Task009Impl.java
@@ -0,0 +1,23 @@
+package com.epam.university.java.core.task009;
+
+import java.io.*;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Task009Impl implements Task009 {
+    @Override
+    public Collection<String> countWords(File sourceFile) {
+        Set<String> words = new HashSet<>();
+        try {
+            Scanner in = new Scanner(sourceFile);
+            while (in.hasNext()) {
+                words.add(in.next());
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return words;
+    }
+}
