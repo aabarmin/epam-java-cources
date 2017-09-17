36e2d2ff83f85ee4f97d583a44cc6cc386107824
Alex Labadin <avlabadin@gmail.com>
2017-09-17 11:22:39 +0300

    TaskTask 001 & Task 002

---
+++ src/main/java/com/epam/university/java/core/task001/Task001Impl.java
@@ -0,0 +1,70 @@
+package com.epam.university.java.core.task001;
+
+/**
+ * Created by Doomsday Device on 17.09.2017.
+ */
+public class Task001Impl implements Task001 {
+    @Override
+    public double addition(String firstNumber, String secondNumber) {
+        double result;
+        if (firstNumber == null && secondNumber == null) {
+            throw new IllegalArgumentException();
+        }
+
+        try {
+            result = Double.parseDouble(firstNumber) + Double.parseDouble(secondNumber);
+        } catch (Exception e) {
+            throw new NumberFormatException();
+        }
+
+        return result;
+    }
+
+    @Override
+    public double subtraction(String firstNumber, String secondNumber) {
+        double result;
+        if (firstNumber == null && secondNumber == null) {
+            throw new IllegalArgumentException();
+        }
+
+        try {
+            result = Double.parseDouble(firstNumber) - Double.parseDouble(secondNumber);
+        } catch (Exception e) {
+            throw new NumberFormatException();
+        }
+
+        return result;
+    }
+
+    @Override
+    public double multiplication(String firstNumber, String secondNumber) {
+        double result;
+        if (firstNumber == null && secondNumber == null) {
+            throw new IllegalArgumentException();
+        }
+
+        try {
+            result = Double.parseDouble(firstNumber) * Double.parseDouble(secondNumber);
+        } catch (Exception e) {
+            throw new NumberFormatException();
+        }
+
+        return result;
+    }
+
+    @Override
+    public double division(String firstNumber, String secondNumber) {
+        double result;
+        if (firstNumber == null && secondNumber == null) {
+            throw new IllegalArgumentException();
+        }
+
+        try {
+            result = Double.parseDouble(firstNumber) / Double.parseDouble(secondNumber);
+        } catch (Exception e) {
+            throw new NumberFormatException();
+        }
+
+        return result;
+    }
+}
