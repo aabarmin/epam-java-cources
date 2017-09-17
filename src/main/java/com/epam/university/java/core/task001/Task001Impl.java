5e5ba6397d5b77b2d68adc41d40e0446a12f3209
Alex Labadin <avlabadin@gmail.com>
2017-09-17 13:34:24 +0300

    Task001

--- src/main/java/com/epam/university/java/core/task001/Task001Impl.java
+++ src/main/java/com/epam/university/java/core/task001/Task001Impl.java
@@ -2,6 +2,9 @@ package com.epam.university.java.core.task001;
 
 /**
  * Created by Doomsday Device on 17.09.2017.
+ * <p>
+ *     Task001
+ * </p>
  */
 public class Task001Impl implements Task001 {
     @Override
