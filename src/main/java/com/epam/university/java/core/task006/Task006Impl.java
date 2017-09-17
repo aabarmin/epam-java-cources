bd322b6b88b43421bf9d769cafbba3922f35a15e
a-sakhnenko <aksakhnenko@gmail.com>
2017-09-17 17:00:05 +0300

    Some style fixes

--- src/main/java/com/epam/university/java/core/task006/Task006Impl.java
+++ src/main/java/com/epam/university/java/core/task006/Task006Impl.java
@@ -4,9 +4,13 @@ import java.util.Collection;
 
 public class Task006Impl implements Task006 {
     @Override
-    strictfp public double resistance(Collection<Measurement> measurements) {
-        if (measurements == null) throw new IllegalArgumentException();
-        if (measurements.isEmpty()) return 0;
+     public strictfp double resistance(Collection<Measurement> measurements) {
+        if (measurements == null) {
+            throw new IllegalArgumentException();
+        }
+        if (measurements.isEmpty()) {
+            return 0;
+        }
 
         double resistance = 0;
         double amperesPerSquare = 0;
@@ -27,7 +31,9 @@ public class Task006Impl implements Task006 {
                     * (measurement.getAmperage() - averageAmp);
         }
         resistance /= amperesPerSquare;
-        if (amperesPerSquare == 0) return 0;
-        return ((int) (resistance*1000))/1000.0;
+        if (amperesPerSquare == 0) {
+            return 0;
+        }
+        return ((int) (resistance * 1000)) / 100.0;
     }
 }
