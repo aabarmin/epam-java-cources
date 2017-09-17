ae9f38e5af26eb95c4e71e85a179203dfaa08335
a-sakhnenko <aksakhnenko@gmail.com>
2017-09-17 16:07:20 +0300

    Task007 is done

--- src/main/java/com/epam/university/java/core/task007/Task007Impl.java
+++ src/main/java/com/epam/university/java/core/task007/Task007Impl.java
@@ -5,8 +5,7 @@ import java.util.Collection;
 
 public class Task007Impl implements Task007 {
     @Override
-    public Collection<Integer> multiplyPolynomial(Collection<Integer> first,
-                                                  Collection<Integer> second) {
+    public Collection<Integer> multiplyPolynomial(Collection<Integer> first, Collection<Integer> second) {
         Collection<Integer> mul = new ArrayList<>();
         Integer[][] mulArray = new Integer[first.size()][second.size()];
         int i = 0;
@@ -14,7 +13,7 @@ public class Task007Impl implements Task007 {
         for (Integer firstCoefficient : first) {
             j = 0;
             for (Integer secondCoefficient : second) {
-                mulArray[i][j++] = firstCoefficient * secondCoefficient;
+                  mulArray[i][j++] = firstCoefficient*secondCoefficient;
             }
             i++;
         }
@@ -22,12 +21,10 @@ public class Task007Impl implements Task007 {
         int[] output = new int[first.size() + second.size() - 1];
         for (i = 0; i < first.size(); i++) {
             for (j = 0; j < second.size(); j++) {
-                output[i + j] += mulArray[i][j];
+                output[i+j] += mulArray[i][j];
             }
         }
-        for (i = 0; i < output.length; i++) {
-            mul.add(output[i]);
-        }
+        for (i = 0; i < output.length ; i++ ) mul.add(output[i]);
 
         return mul;
 
