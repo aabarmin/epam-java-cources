64fed0a0f39a4f3fd3901a815b42f0e22e0d5726
stelmakhmaksim <stelmakh96@mail.ru>
2017-09-18 17:32:25 +0300

    Some checkstyle corrections.

--- src/main/java/com/epam/university/java/core/task010/Task010Impl.java
+++ src/main/java/com/epam/university/java/core/task010/Task010Impl.java
@@ -1,6 +1,10 @@
 package com.epam.university.java.core.task010;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -9,9 +13,9 @@ public class Task010Impl implements Task010 {
     public Map<String, Integer> countWordNumbers(File source) {
         String[] strArr = null;
         try (BufferedReader br = new BufferedReader(new InputStreamReader(
-                new FileInputStream(source)))){
+                new FileInputStream(source)))) {
             strArr = br.readLine().toLowerCase().split("[^a-zA-Z0-9_’-]+");
-        }catch(IOException e){
+        } catch (IOException e) {
             System.out.print("Input-output error happened!" + e);
         }
         Map<String, Integer> map = new HashMap<>();
