381a9bdd6dbf22a11710685e07915d9c44d876dc
stelmakhmaksim <stelmakh96@mail.ru>
2017-09-18 17:32:25 +0300

    Some checkstyle corrections.

--- src/main/java/com/epam/university/java/core/task008/Task008Impl.java
+++ src/main/java/com/epam/university/java/core/task008/Task008Impl.java
@@ -3,7 +3,6 @@ package com.epam.university.java.core.task008;
 public class Task008Impl implements Task008 {
     @Override
     public boolean isValid(String sourceString) {
-
         sourceString = sourceString.replaceAll("[^\\[\\]\\(\\)\\{\\}]+", "");
         char[] charArray = sourceString.toCharArray();
         MyStack<Character> stack = new MyStack<>(charArray.length);
