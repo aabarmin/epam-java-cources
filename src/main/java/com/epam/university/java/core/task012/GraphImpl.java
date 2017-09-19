4d90883d744d90ad11657cf342aec2cd6004e5a3
a-sakhnenko <aksakhnenko@gmail.com>
2017-09-20 00:19:10 +0300

    Task012 is done, style is checked

--- src/main/java/com/epam/university/java/core/task012/GraphImpl.java
+++ src/main/java/com/epam/university/java/core/task012/GraphImpl.java
@@ -7,7 +7,7 @@ public class GraphImpl implements Graph {
     private int vertexCount;
     private boolean[][] isEdgeExist;
 
-    public GraphImpl(int vertexCount) {
+    GraphImpl(int vertexCount) {
         this.vertexCount = ++vertexCount;
         isEdgeExist = new boolean[vertexCount][vertexCount];
     }
