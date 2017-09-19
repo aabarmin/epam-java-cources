af1a988efafc1d7a512de5145654c5a4ffa574c6
Aleksandr Barmin <aleksandr_barmin@epam.com>
2017-09-19 13:03:46 +0300

    Task015 fix

--- src/main/java/com/epam/university/java/core/task015/Point.java
+++ src/main/java/com/epam/university/java/core/task015/Point.java
@@ -8,23 +8,23 @@ public interface Point {
      * Get x value of point.
      * @return value
      */
-    int getX();
+    double getX();
 
     /**
      * Get y value of point.
      * @return value
      */
-    int getY();
+    double getY();
 
     /**
      * Set x value of point.
      * @param x value
      */
-    void setX(int x);
+    void setX(double x);
 
     /**
      * Set y value of point.
      * @param y value
      */
-    void setY(int y);
+    void setY(double y);
 }
