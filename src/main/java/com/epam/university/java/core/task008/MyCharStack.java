2ea0cbf5fcd09c9ff51397be5043d95ee5a685d8
a-sakhnenko <aksakhnenko@gmail.com>
2017-09-19 20:36:19 +0300

    Style warnings is fixed

--- src/main/java/com/epam/university/java/core/task008/MyCharStack.java
+++ src/main/java/com/epam/university/java/core/task008/MyCharStack.java
@@ -17,6 +17,7 @@ class MyCharStack {
         elementCount++;
         items.append(item);
     }
+
     char pop() {
         char item = items.charAt(elementCount - 1);
         items.deleteCharAt(--elementCount);
