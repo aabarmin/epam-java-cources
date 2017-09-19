fed0c56476a3281d4893b67eb3fb148d2bcd7986
a-sakhnenko <aksakhnenko@gmail.com>
2017-09-19 22:24:56 +0300

    Task011 is done, style is checked

--- src/main/java/com/epam/university/java/core/task011/Task011Impl.java
+++ src/main/java/com/epam/university/java/core/task011/Task011Impl.java
@@ -47,13 +47,19 @@ public class Task011Impl implements Task011 {
 
     @Override
     public String getLastName(LinkedList<String> collection) {
-        String person = null;
-        while (!collection.isEmpty()) {
-            person = collection.removeFirst();
-            if (!collection.isEmpty()) {
-                collection.add(collection.remove());
+        String stayPerson = null;
+        int i = 0;
+        while (true) {
+            collection.remove(i);
+            if (i == collection.size()) {
+                i = 0;
             }
+            if (collection.get(i).equals(stayPerson)) {
+                break;
+            }
+            stayPerson = collection.get(i);
+            i = (i + 1) < collection.size() ? (i + 1) : 0;
         }
-        return person;
+        return stayPerson;
     }
 }
