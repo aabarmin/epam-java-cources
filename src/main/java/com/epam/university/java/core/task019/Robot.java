d70a59227d7e232d5e9f4ed8b40d112c9128c4fc
Aleksandr Barmin <aleksandr_barmin@epam.com>
2017-09-20 10:30:09 +0300

    Task 019 added

--- src/main/java/com/epam/university/java/core/task019/Robot.java
+++ src/main/java/com/epam/university/java/core/task019/Robot.java
@@ -15,10 +15,4 @@ public interface Robot {
      * @param position position
      */
     void setPosition(RobotPosition position);
-
-    /**
-     * Invoke robot command.
-     * @param command command to invoke
-     */
-    void invokeAction(RobotCommand command);
 }
