5b09419e46a0426ff70e07c8669d69b1b1882b84
FruitAss <fruit9433@yandex.ru>
2017-09-21 13:27:41 +0200

    Merge branch 'TTskhovrebov' of C:\Users\Mikt\Desktop\java_core\epam-java-cources with conflicts.

--- src/main/java/com/epam/university/java/core/task001/Task001Impl.java
+++ src/main/java/com/epam/university/java/core/task001/Task001Impl.java
@@ -6,7 +6,6 @@ public class Task001Impl implements Task001 {
 
     @Override
     public double addition(String firstNumber, String secondNumber) {
-<<<<<<< HEAD
         if ((firstNumber == null&&secondNumber == null) ||
                 (Objects.equals(firstNumber, " ") && Objects.equals(secondNumber, " "))) {
             throw new IllegalArgumentException();
@@ -18,24 +17,10 @@ public class Task001Impl implements Task001 {
         else {
             return Integer.parseInt(firstNumber.trim())+Integer.parseInt(secondNumber.trim());
         }
-=======
-
-
-        if((firstNumber==null&&secondNumber==null)||(Objects.equals(firstNumber, " ") && Objects.equals(secondNumber, " "))){
-            throw new IllegalArgumentException();
-        }
-        else if(firstNumber.matches("^[a-zA-z_-]$")||secondNumber.matches("^[a-zA-z_-]$"))
-        {
-            throw new NumberFormatException();
-        }
-
-        return Integer.parseInt(firstNumber.trim())+Integer.parseInt(secondNumber.trim());
->>>>>>> c27b7c3fd124118495fb597fdb747a97889106ce
     }
 
     @Override
     public double subtraction(String firstNumber, String secondNumber) {
-<<<<<<< HEAD
         if ((firstNumber==null&&secondNumber == null) ||
                 (Objects.equals(firstNumber, " ") && Objects.equals(secondNumber, " "))) {
             throw new IllegalArgumentException();
@@ -46,23 +31,10 @@ public class Task001Impl implements Task001 {
         else {
             return Integer.parseInt(firstNumber.trim()) - Integer.parseInt(secondNumber.trim());
         }
-=======
-
-        if((firstNumber==null&&secondNumber==null)||(Objects.equals(firstNumber, " ") && Objects.equals(secondNumber, " "))){
-            throw new IllegalArgumentException();
-        }
-        else if(firstNumber.matches("^[a-zA-z_-]$")||secondNumber.matches("^[a-zA-z_-]$"))
-        {
-            throw new NumberFormatException();
-        }
-
-        return Integer.parseInt(firstNumber.trim())-Integer.parseInt(secondNumber.trim());
->>>>>>> c27b7c3fd124118495fb597fdb747a97889106ce
     }
 
     @Override
     public double multiplication(String firstNumber, String secondNumber) {
-<<<<<<< HEAD
         if ((firstNumber == null&&secondNumber == null) ||
                 (Objects.equals(firstNumber, " ") && Objects.equals(secondNumber, " "))) {
             throw new IllegalArgumentException();
@@ -73,26 +45,10 @@ public class Task001Impl implements Task001 {
         else {
             return Double.parseDouble(firstNumber.trim())*Double.parseDouble(secondNumber.trim());
         }
-=======
-
-        if((firstNumber==null&&secondNumber==null)||(Objects.equals(firstNumber, " ") && Objects.equals(secondNumber, " "))){
-            throw new IllegalArgumentException();
-        }
-        else if(firstNumber.matches("^[a-zA-z_-]$")||secondNumber.matches("^[a-zA-z_-]$"))
-        {
-            throw new NumberFormatException();
-        }
-        else{
-            return Double.parseDouble(firstNumber.trim())*Double.parseDouble(secondNumber.trim());
-        }
-
-
->>>>>>> c27b7c3fd124118495fb597fdb747a97889106ce
     }
 
     @Override
     public double division(String firstNumber, String secondNumber) {
-<<<<<<< HEAD
         if ((firstNumber==null&&secondNumber == null) ||
                 (Objects.equals(firstNumber, " ") && Objects.equals(secondNumber, " "))) {
             throw new IllegalArgumentException();
@@ -110,25 +66,6 @@ public class Task001Impl implements Task001 {
             return Double.NEGATIVE_INFINITY;
         }
         else {
-=======
-        if((firstNumber==null&&secondNumber==null)||(Objects.equals(firstNumber, " ") && Objects.equals(secondNumber, " "))){
-            throw new IllegalArgumentException();
-        }
-        else if(firstNumber.matches("^[a-zA-z_-]$")||secondNumber.matches("^[a-zA-z_-]$"))
-        {
-            throw new NumberFormatException();
-        }
-        else if(secondNumber.trim().equals("0")&&firstNumber.trim().equals("0")){
-            return Double.NaN;
-        }
-        else if(secondNumber.trim().equals("0")&&firstNumber.trim().equals("1")){
-            return Double.POSITIVE_INFINITY;
-        }
-        else if(secondNumber.trim().equals("0")&&firstNumber.trim().equals("-1")){
-            return Double.NEGATIVE_INFINITY;
-        }
-        else{
->>>>>>> c27b7c3fd124118495fb597fdb747a97889106ce
             return Double.parseDouble(firstNumber.trim())/Double.parseDouble(secondNumber.trim());
         }
     }
--- src/main/java/com/epam/university/java/core/task002/Task002Impl.java
+++ src/main/java/com/epam/university/java/core/task002/Task002Impl.java
@@ -3,7 +3,6 @@ package com.epam.university.java.core.task002;
 public class Task002Impl implements Task002 {
     @Override
     public boolean isEquals(String firstString, String secondString) {
-<<<<<<< HEAD
         if ((firstString == null || secondString == null)) {
             throw new IllegalArgumentException();
         }
@@ -11,22 +10,12 @@ public class Task002Impl implements Task002 {
             return true;
         }
         else {
-=======
-        if((firstString==null||secondString==null)){
-            throw new IllegalArgumentException();
-        }
-        else if(firstString.equals("")||secondString.equals("")){
-            return true;
-        }
-        else{
->>>>>>> c27b7c3fd124118495fb597fdb747a97889106ce
             return firstString.equals(secondString);
         }
     }
 
     @Override
     public String left(String sourceString, int number) {
-<<<<<<< HEAD
         if (sourceString == null) {
             throw new IllegalArgumentException();
         }
@@ -39,44 +28,22 @@ public class Task002Impl implements Task002 {
             }
             else {
                 return sourceString.substring(0, number);
-=======
-        if(sourceString==null){
-            throw new IllegalArgumentException();
-        }
-        else if(number<0){
-            throw new IllegalArgumentException();
-        }
-        else{
-            if(number>sourceString.length()){
-                return sourceString;
-            }
-            else{
-                return sourceString.substring(0,number);
->>>>>>> c27b7c3fd124118495fb597fdb747a97889106ce
             }
         }
     }
 
     @Override
     public String left(String sourceString, String separator) {
-<<<<<<< HEAD
         if (sourceString == null || separator == null) {
             throw new IllegalArgumentException();
         }
         else {
-=======
-        if(sourceString==null||separator==null){
-            throw new IllegalArgumentException();
-        }
-        else{
->>>>>>> c27b7c3fd124118495fb597fdb747a97889106ce
             return sourceString.split(separator)[0];
         }
     }
 
     @Override
     public String right(String sourceString, int number) {
-<<<<<<< HEAD
         if (sourceString == null) {
             throw new IllegalArgumentException();
         }
@@ -89,71 +56,36 @@ public class Task002Impl implements Task002 {
             }
             else {
                 return sourceString.substring(sourceString.length()-5, sourceString.length());
-=======
-        if(sourceString==null){
-            throw new IllegalArgumentException();
-        }
-        else if(number<0){
-            throw new IllegalArgumentException();
-        }
-        else{
-            if(number>sourceString.length()){
-                return sourceString;
-            }
-            else{
-                return sourceString.substring(sourceString.length()-5,sourceString.length());
->>>>>>> c27b7c3fd124118495fb597fdb747a97889106ce
             }
         }
     }
 
     @Override
     public String right(String sourceString, String separator) {
-<<<<<<< HEAD
         if (sourceString == null || separator == null) {
             throw new IllegalArgumentException();
         }
         else {
-=======
-        if(sourceString==null||separator==null){
-            throw new IllegalArgumentException();
-        }
-        else{
->>>>>>> c27b7c3fd124118495fb597fdb747a97889106ce
             return sourceString.split(separator)[1];
         }
     }
 
     @Override
     public String[] split(String sourceString, String split) {
-<<<<<<< HEAD
         if (sourceString == null || split == null) {
             throw new IllegalArgumentException();
         }
         else {
-=======
-        if(sourceString==null||split==null){
-            throw new IllegalArgumentException();
-        }
-        else{
->>>>>>> c27b7c3fd124118495fb597fdb747a97889106ce
             return sourceString.split(split);
         }
     }
 
     @Override
     public String join(String[] sourceCollection, String glue) {
-<<<<<<< HEAD
         if (sourceCollection == null || glue == null) {
             throw new IllegalArgumentException();
         }
         else {
-=======
-        if(sourceCollection==null||glue==null){
-            throw new IllegalArgumentException();
-        }
-        else{
->>>>>>> c27b7c3fd124118495fb597fdb747a97889106ce
             return sourceCollection[0]+glue+sourceCollection[1];
         }
     }
