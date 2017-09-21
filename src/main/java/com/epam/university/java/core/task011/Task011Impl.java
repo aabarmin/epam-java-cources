d3de1fb954c86ed42763e94f8a9b0e1d5ebc2c74
stelmakhmaksim <stelmakh96@mail.ru>
2017-09-21 18:42:56 +0300

    Task 11 is done. All tests are passed.

---
+++ src/main/java/com/epam/university/java/core/task011/Task011Impl.java
@@ -0,0 +1,61 @@
+package com.epam.university.java.core.task011;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class Task011Impl implements Task011 {
+    @Override
+    public String getLastName(String[] collection) {
+        if (collection == null) {
+            throw new IllegalArgumentException("Array not provided!");
+        }
+        int i = 0;
+        int count = 0;
+        while (true) {
+            collection[i] = null;
+            i += 2;
+            count++;
+            if (i >= collection.length) {
+                i %= collection.length;
+                String[] intermediateStr = new String[collection.length - count];
+                for (int j = 0, k = 0; j < collection.length; j++) {
+                    if (collection[j] != null) {
+                        intermediateStr[k++] = collection[j];
+                    }
+                }
+                collection = intermediateStr;
+                if (collection.length == 1) {
+                    return collection[0];
+                }
+                count = 0;
+            }
+        }
+    }
+
+    @Override
+    public String getLastName(ArrayList<String> collection) {
+        if (collection == null) {
+            throw new IllegalArgumentException("Array not provided!");
+        }
+        return getLastName(new LinkedList<>(collection));
+    }
+
+
+    @Override
+    public String getLastName(LinkedList<String> collection) {
+        if (collection == null) {
+            throw new IllegalArgumentException("Array not provided!");
+        }
+        int i = 0;
+        while (true) {
+            collection.remove(i);
+            i++;
+            if (i >= collection.size()) {
+                i %= collection.size();
+            }
+            if (collection.size() == 1) {
+                return collection.get(0);
+            }
+        }
+    }
+}
