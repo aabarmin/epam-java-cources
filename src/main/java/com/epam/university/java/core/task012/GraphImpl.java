29375b0bfc875bc46339e628f97ea029ed7e426d
stelmakhmaksim <stelmakh96@mail.ru>
2017-09-22 12:37:53 +0300

    Files with task 12 & 17 have been added.

---
+++ src/main/java/com/epam/university/java/core/task012/GraphImpl.java
@@ -0,0 +1,25 @@
+package com.epam.university.java.core.task012;
+
+import java.util.Collection;
+
+public class GraphImpl implements Graph {
+    @Override
+    public void createEdge(int from, int to) {
+
+    }
+
+    @Override
+    public boolean edgeExists(int from, int to) {
+        return false;
+    }
+
+    @Override
+    public void removeEdge(int from, int to) {
+
+    }
+
+    @Override
+    public Collection<Integer> getAdjacent(int from) {
+        return null;
+    }
+}
---
+++ src/main/java/com/epam/university/java/core/task012/Task012Impl.java
@@ -0,0 +1,15 @@
+package com.epam.university.java.core.task012;
+
+import java.util.Collection;
+
+public class Task012Impl implements Task012 {
+    @Override
+    public Graph invokeActions(Graph sourceGraph, Collection<GraphAction> actions) {
+        return null;
+    }
+
+    @Override
+    public boolean pathExists(Graph graph, int from, int to) {
+        return false;
+    }
+}
