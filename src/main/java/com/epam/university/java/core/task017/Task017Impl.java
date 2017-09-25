9778c47c81a98d12be16f134a4e2e72f8750cdf2
stelmakhmaksim <stelmakh96@mail.ru>
2017-09-25 15:33:18 +0300

    Added several checks.

--- src/main/java/com/epam/university/java/core/task017/Task017Impl.java
+++ src/main/java/com/epam/university/java/core/task017/Task017Impl.java
@@ -3,16 +3,25 @@ package com.epam.university.java.core.task017;
 public class Task017Impl implements Task017 {
     @Override
     public String formatString(Object... args) {
+        if (args.length != 2 || args == null) {
+            throw new IllegalArgumentException("Two arguments are required!");
+        }
         return String.format("You know %s, %s!", args);
     }
 
     @Override
     public String formatNumbers(Object... args) {
+        if (args.length != 1 || args == null) {
+            throw new IllegalArgumentException("One argument are required!");
+        }
         return String.format("%.1f, %<.2f, %+<.2f, %<a", args);
     }
 
     @Override
     public String formatDates(Object... args) {
+        if (args.length != 1 || args == null) {
+            throw new IllegalArgumentException("One argument are required!");
+        }
         return String.format("%tY.%<td.%<tm", args);
     }
 }
