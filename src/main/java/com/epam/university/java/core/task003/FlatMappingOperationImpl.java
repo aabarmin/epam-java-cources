499a0bf089c8e1e15ff6b9cfe71bb1ba88d61461
DevRedFA <Sedykhivan@gmail.com>
2017-09-26 22:56:00 +0300

    Updated test for Task011.

--- src/main/java/com/epam/university/java/core/task003/FlatMappingOperationImpl.java
+++
@@ -1,9 +0,0 @@
-package com.epam.university.java.core.task003;
-
-
-public class FlatMappingOperationImpl implements FlatMappingOperation {
-    @Override
-    public String[] flatMap(String source) {
-        return source.split(",");
-    }
-}
