92205dacd7ea2ac13bed7a3046c406a8f8ac2ae3
stelmakhmaksim <stelmakh96@mail.ru>
2017-09-27 10:44:01 +0300

    Task 19 is done. All tests are passed.

--- src/main/java/com/epam/university/java/core/task019/RobotImpl.java
+++ src/main/java/com/epam/university/java/core/task019/RobotImpl.java
@@ -1,4 +1,82 @@
 package com.epam.university.java.core.task019;
 
-public class RobotImpl {
+public class RobotImpl implements Robot {
+    private RobotPosition position = new RobotPositionImpl();
+    private Direction direction = Direction.SOUTH;
+
+    @Override
+    public RobotPosition getPosition() {
+        return position;
+    }
+
+    @Override
+    public void setPosition(RobotPosition position) {
+        this.position = position;
+    }
+
+    @Override
+    public void invokeAction(RobotCommand command) {
+        switch (command) {
+            case TURN_LEFT:
+                switch (direction) {
+                    case SOUTH:
+                        direction = Direction.EAST;
+                        break;
+                    case NORTH:
+                        direction = Direction.WEST;
+                        break;
+                    case WEST:
+                        direction = Direction.SOUTH;
+                        break;
+                    case EAST:
+                        direction = Direction.NORTH;
+                        break;
+                    default:
+                        System.out.println("Incorrect direction!");
+                        break;
+                }
+                break;
+            case TURN_RIGHT:
+                switch (direction) {
+                    case SOUTH:
+                        direction = Direction.WEST;
+                        break;
+                    case NORTH:
+                        direction = Direction.EAST;
+                        break;
+                    case WEST:
+                        direction = Direction.NORTH;
+                        break;
+                    case EAST:
+                        direction = Direction.SOUTH;
+                        break;
+                    default:
+                        System.out.println("Incorrect direction!");
+                        break;
+                }
+                break;
+            case MOVE_FORWARD:
+                switch (direction) {
+                    case SOUTH:
+                        position.setY(position.getY() - 1);
+                        break;
+                    case NORTH:
+                        position.setY(position.getY() + 1);
+                        break;
+                    case WEST:
+                        position.setX(position.getX() - 1);
+                        break;
+                    case EAST:
+                        position.setX(position.getX() + 1);
+                        break;
+                    default:
+                        System.out.println("Incorrect direction!");
+                        break;
+                }
+                break;
+            default:
+                System.out.println("Incorrect action!");
+                break;
+        }
+    }
 }
--- src/main/java/com/epam/university/java/core/task019/RobotPositionImpl.java
+++ src/main/java/com/epam/university/java/core/task019/RobotPositionImpl.java
@@ -1,4 +1,26 @@
 package com.epam.university.java.core.task019;
 
-public class RobotPositionImpl {
+public class RobotPositionImpl implements RobotPosition {
+    private int coordX;
+    private int coordY;
+
+    @Override
+    public int getX() {
+        return coordX;
+    }
+
+    @Override
+    public int getY() {
+        return coordY;
+    }
+
+    @Override
+    public void setX(int x) {
+        coordX = x;
+    }
+
+    @Override
+    public void setY(int y) {
+        coordY = y;
+    }
 }
