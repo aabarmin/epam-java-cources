44065c9bd308ff3e4449867e21b9196135e9f258
ikiselev7 <ikiselev7@gmail.com>
2017-09-28 00:00:21 +0300

    Tasks 24-25

---
+++ src/main/java/com/epam/university/java/core/task025/Task025Impl.java
@@ -0,0 +1,23 @@
+package com.epam.university.java.core.task025;
+
+/**
+ * Created by ilya on 27.09.17.
+ */
+public class Task025Impl implements Task025 {
+
+    @Override
+    public int getAmountOfAlteredLetters(String sourceMessage) {
+        final String sos = "SOS";
+
+        String[] letters = sourceMessage.split("");
+
+        int count = 0;
+        for (int i = 0; i < letters.length; i++) {
+            if (!letters[i].equals(Character.toString(sos.charAt(i % 3)))) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
