b4cc02030874d69a84ebc567c9321d125747ef7d
Titov Sergei <quosapv@yandex.ru>
2017-09-27 11:15:17 +0300

    Task018 done

--- src/main/java/com/epam/university/java/core/task018/Task018.java
+++ src/main/java/com/epam/university/java/core/task018/Task018.java
@@ -5,7 +5,7 @@ package com.epam.university.java.core.task018;
  */
 public interface Task018 {
     /**
-     * Check is annotation present in the following object.
+     * Check if annotation presents in the following object.
      * @param toCheck object to check
      * @param annotationToFind annotation to look for
      * @return is annotation presents
