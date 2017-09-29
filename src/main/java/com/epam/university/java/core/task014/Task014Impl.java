4b77da257a6918c3dd6fb3cf8c709a73e858e1fb
Mikhail Chtetsov <chtetsoff@gmail.com>
2017-09-29 13:43:11 +0300

    Task014

---
+++ src/main/java/com/epam/university/java/core/task014/Task014Impl.java
@@ -0,0 +1,59 @@
+package com.epam.university.java.core.task014;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+
+/**
+ * {@inheritDoc}
+ */
+public class Task014Impl implements Task014 {
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Collection<VampireNumber> getVampireNumbers() {
+        Collection<VampireNumber> listOfVNumbers = new HashSet<>();
+        int result = 0;
+
+        for (int i = 11; i < 99; i++) {
+            for (int j = 11; j < 99; j++) {
+                result = i * j;
+                if (result < 1000) {
+                    continue;
+                }
+                if (result > 9999) {
+                    break;
+                }
+                if (isVampire(result, i, j)) {
+                    listOfVNumbers.add(new VampireNumberImpl(result, i, j));
+                }
+            }
+        }
+        return listOfVNumbers;
+    }
+
+    /**
+     * Check if number is vampire.
+     *
+     * @param mult   result of multiplication of first and second
+     * @param first  first multiplier
+     * @param second second multiplier
+     * @return
+     */
+    private boolean isVampire(int mult, int first, int second) {
+        List<String> multList = Arrays.asList(String.valueOf(mult).split(""));
+        List<String> removeList = new ArrayList<>();
+
+        removeList.add(first / 10 + "");
+        removeList.add(first % 10 + "");
+        removeList.add(second / 10 + "");
+        removeList.add(second % 10 + "");
+        Collections.sort(multList);
+        Collections.sort(removeList);
+        return multList.equals(removeList);
+    }
+}
---
+++ src/main/java/com/epam/university/java/core/task014/VampireNumberImpl.java
@@ -0,0 +1,62 @@
+package com.epam.university.java.core.task014;
+
+
+/**
+ * {@inheritDoc}
+ */
+public class VampireNumberImpl implements VampireNumber {
+    private int multiplication;
+    private int first;
+    private int second;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        VampireNumberImpl that = (VampireNumberImpl) o;
+
+        return multiplication == that.multiplication;
+    }
+
+    @Override
+    public int hashCode() {
+        return multiplication;
+    }
+
+    /**
+     * Create new instance of Vampire Number with designed figures
+     * @param multiplication Vampire number
+     * @param first first multiplier
+     * @param second second multiplier
+     */
+    protected VampireNumberImpl(int multiplication, int first, int second) {
+        this.multiplication = multiplication;
+        this.first = first;
+        this.second = second;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public int getMultiplication() {
+        return multiplication;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public int getFirst() {
+        return first;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public int getSecond() {
+        return second;
+    }
+}
