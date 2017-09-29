ac2874156806d8be2db047a467e6115df99ca87c
DoggyDoge <agadullinvadim@yandex.ru>
2017-09-29 21:51:43 +0300

    Add files via upload
    Почему ошибка в тесте NullPointerException??
    почему в сете на 1 элемент больше?

--- src/main/java/com/epam/university/java/core/task009/TestClass.java
+++ src/main/java/com/epam/university/java/core/task009/TestClass.java
@@ -2,7 +2,9 @@ package com.epam.university.java.core.task009;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
@@ -13,20 +15,28 @@ public class TestClass {
         Scanner input = new Scanner(sourceFile);
 
         Set<String> words = new HashSet<>();
-        int count = 0;
+
+        String temp;
+
+
+        ArrayList<String> list = new ArrayList<>();
 
         while (input.hasNext()){
-            words.add(input.next());
-            count++;
+            temp = input.next();
+            //String[] s = temp.toLowerCase().split("\\pP");
+            String[] s = temp.toLowerCase().split("[-,.:!?’]");
+            for (int i = 0; i < s.length; i++){
+                //System.out.println(s[i]);
+                words.add(s[i]);
+            }
         }
-        System.out.println(count);
+        System.out.println(words.size());
         return words;
     }
 
     public static void main(String[] args) throws FileNotFoundException {
-        for (String element: countWords(new File("C:\\Users\\Zstudent\\epam-java-cources\\src\\main\\java\\com\\epam\\university\\java\\core\\task009\\words.txt"))) {
+        for (String element: countWords(new File("C:\\Users\\Zstudent\\epam-java-cources\\src\\main\\resources\\task009\\words.txt"))) {
             System.out.println(element);
         }
-
     }
 }
