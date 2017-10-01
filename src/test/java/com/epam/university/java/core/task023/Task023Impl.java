c843e6b60079a5b97a8d077ee4630f770e2ba1e1
Titov Sergei <quosapv@yandex.ru>
2017-10-01 14:58:08 +0300

    Task023 done

---
+++ src/test/java/com/epam/university/java/core/task023/Task023Impl.java
@@ -0,0 +1,32 @@
+package com.epam.university.java.core.task023;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Implementation class for Task022.
+ *
+ * @author Sergei Titov
+ */
+public class Task023Impl implements Task023 {
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String extract(String phoneString) throws IllegalArgumentException {
+
+        final Pattern pattern = Pattern.compile(
+                "(\\d\\d\\d)[ )-]*[\\d][ -]*[\\d][ -]*[\\d][ -]*[\\d][ -]*[\\d][ -]*[\\d][ -]*[\\d]$"
+        );
+
+        Matcher matcher = pattern.matcher(phoneString);
+
+        if (matcher.find()) {
+            return matcher.group(1);
+        }
+        else {
+            throw new IllegalArgumentException();
+        }
+    }
+}
