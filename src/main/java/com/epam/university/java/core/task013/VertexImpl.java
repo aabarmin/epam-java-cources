4fce7e56f6cf04efadefc471135fc10dd4b48427
FruitAss <fruit9433@yandex.ru>
2017-10-02 14:06:36 +0200

    +Task013

--- src/main/java/com/epam/university/java/core/task013/VertexImpl.java
+++ src/main/java/com/epam/university/java/core/task013/VertexImpl.java
@@ -1,4 +1,25 @@
 package com.epam.university.java.core.task013;
 
-public class VertexImpl {
+public class VertexImpl implements Vertex {
+    int x;
+    int y;
+    @Override
+    public int getX() {
+        return x;
+    }
+
+    @Override
+    public void setX(int value) {
+        this.x = value;
+    }
+
+    @Override
+    public int getY() {
+        return y;
+    }
+
+    @Override
+    public void setY(int value) {
+        this.y = value;
+    }
 }
