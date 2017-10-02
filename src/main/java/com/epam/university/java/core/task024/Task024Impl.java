1914a891263ca37ae23219c71141752d492a3c66
Titov Sergei <quosapv@yandex.ru>
2017-10-02 15:31:21 +0300

    Task024 done

---
+++ src/main/java/com/epam/university/java/core/task024/Task024Impl.java
@@ -0,0 +1,37 @@
+package com.epam.university.java.core.task024;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Implementation class for Task022.
+ *
+ * @author Sergei Titov
+ */
+public class Task024Impl implements Task024 {
+
+    private static final String regExp = "([\\w][^\\p{Lu}]*)";
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Collection<String> getWordsCount(String source) {
+
+        Collection<String> collection = new ArrayList<>();
+
+        final Pattern pattern = Pattern.compile(regExp, Pattern.UNICODE_CHARACTER_CLASS);
+        Matcher matcher = pattern.matcher(source);
+
+        while (matcher.find()) {
+            collection.add(matcher.group(0).toLowerCase());
+            System.out.println(
+                    matcher.group(0)
+            );
+        }
+
+        return collection;
+    }
+}
