253440b7e7464eceae85dd4fb0f49ba53a85a3f6
VeraGrunina <vegrunina@mail.ru>
2017-10-02 14:07:35 +0400

    Task 024

--- src/main/java/com/epam/university/java/core/task024/Task024Impl.java
+++ src/main/java/com/epam/university/java/core/task024/Task024Impl.java
@@ -1,5 +1,6 @@
 package com.epam.university.java.core.task024;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 /**
@@ -8,7 +9,34 @@ import java.util.Collection;
 public class Task024Impl implements Task024 {
     @Override
     public Collection<String> getWordsCount(String source) {
+        if (source.length() == 0) {
+            Collection<String> result = new ArrayList<>();
+            return result;
+        } else {
 
-        return null;
+            String sourceLowerCase = source.toLowerCase();
+
+            char[] sourceChar = source.toCharArray();
+            char[] sourceCharLowerCase = sourceLowerCase.toCharArray();
+
+            Collection<String> result = new ArrayList<>();
+            String s = "" + sourceCharLowerCase[0];
+
+            for (int i = 1; i < sourceChar.length; i++) {
+                if (sourceChar[i] == sourceCharLowerCase[i]) {
+                    s += sourceChar[i];
+                }
+                if (sourceChar[i] != sourceCharLowerCase[i]) {
+                    result.add(s);
+                    s = "";
+                    s += sourceCharLowerCase[i];
+                }
+            }
+            result.add(s);
+
+            //System.out.println(result);
+
+            return result;
+        }
     }
 }
