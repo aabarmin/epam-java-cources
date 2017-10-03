b79cb5324c61c88e628f97f8deb420950477cda3
a-sakhnenko <aksakhnenko@gmail.com>
2017-10-03 20:35:48 +0300

    Task018 is done.

---
+++ src/main/java/com/epam/university/java/core/task018/Task018Impl.java
@@ -0,0 +1,45 @@
+package com.epam.university.java.core.task018;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Task018Impl implements Task018 {
+    @Override
+    public boolean isAnnotationPresent(Object toCheck, Class<?> annotationToFind) {
+        Class clazz = toCheck.getClass();
+        List<Annotation> annotations = new LinkedList<>();
+        if (clazz.getAnnotation(annotationToFind) != null) {
+            return true;
+        }
+
+        for (Field field : clazz.getDeclaredFields()) {
+            annotations.addAll(Arrays.asList(field.getDeclaredAnnotations()));
+        }
+
+        for (Method method : clazz.getDeclaredMethods()) {
+            annotations.addAll(Arrays.asList(method.getDeclaredAnnotations()));
+            for (Parameter parameter : method.getParameters()) {
+                annotations.addAll(Arrays.asList(parameter.getDeclaredAnnotations()));
+            }
+        }
+
+        for (Constructor constructor : clazz.getDeclaredConstructors()) {
+            annotations.addAll(Arrays.asList(constructor.getDeclaredAnnotations()));
+        }
+
+        annotations.addAll(Arrays.asList(clazz.getPackage().getAnnotations()));
+
+        for (Annotation annotation : annotations) {
+            if (annotation.annotationType().equals(annotationToFind)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
