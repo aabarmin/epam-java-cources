0f50e68250ccae746dcbc3aebf678a2d5a481164
razyke <waqk@mail.ru>
2017-10-04 14:14:07 +0300

    Tasks 015 & 21 completed + some style updates.

---
+++ src/main/java/com/epam/university/java/core/task015/SquareFactoryImpl.java
@@ -0,0 +1,9 @@
+package com.epam.university.java.core.task015;
+
+public class SquareFactoryImpl implements SquareFactory {
+
+    @Override
+    public Square newInstance(Point first, Point second) {
+        return new SquareImpl(first,second);
+    }
+}
