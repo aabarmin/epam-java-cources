442408f10970bb7f331d769845695c73025b4f48
stelmakhmaksim <stelmakh96@mail.ru>
2017-10-05 18:49:05 +0300

    Task 27 is done. All tests are passed.

--- src/main/java/com/epam/university/java/core/task027/Task027Impl.java
+++ src/main/java/com/epam/university/java/core/task027/Task027Impl.java
@@ -1,4 +1,51 @@
 package com.epam.university.java.core.task027;
 
-public class Task027Impl {
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class Task027Impl implements Task027 {
+    @Override
+    public Collection<Integer> extract(String sourceString) {
+        if (sourceString == null || sourceString.matches("[^0-9]+")) {
+            throw new IllegalArgumentException();
+        }
+        if ('0' == sourceString.charAt(0) || "".equals(sourceString)) {
+            return Collections.emptyList();
+        }
+        for (int i = 1; i <= sourceString.length() / 2; i++) {
+            List<Integer> extractedIntegers = new ArrayList<>();
+            int currentPosition = 0;
+            int interval = i;
+            extractedIntegers.add(Integer.parseInt(sourceString.substring(currentPosition, interval)));
+            currentPosition += interval;
+
+            while (currentPosition <= sourceString.length() - interval) {
+                int currentNumber = extractedIntegers.get(extractedIntegers.size() - 1);
+                int nextNumber = Integer.parseInt(sourceString.substring(currentPosition, currentPosition + interval));
+
+                if (nextNumber - currentNumber == 1) {
+                    extractedIntegers.add(nextNumber);
+                    currentPosition += interval;
+                } else if (nextNumber - currentNumber < 1) {
+                    interval++;
+                } else {
+                    break;
+                }
+            }
+
+            if (currentPosition >= sourceString.length()) {
+                StringBuilder stringNumbers = new StringBuilder();
+                for (Integer number : extractedIntegers) {
+                    stringNumbers.append(number);
+                }
+                if (stringNumbers.length() != sourceString.length()) {
+                    return Collections.emptyList();
+                }
+                return extractedIntegers;
+            }
+        }
+        return Collections.emptyList();
+    }
 }
