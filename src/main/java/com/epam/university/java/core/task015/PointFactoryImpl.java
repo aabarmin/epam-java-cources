748ddaef11c867ebde2a0ade51391e88f0390a32
FruitAss <fruit9433@yandex.ru>
2017-10-06 10:28:42 +0200

    Tasks 021-025

--- src/main/java/com/epam/university/java/core/task015/PointFactoryImpl.java
+++ src/main/java/com/epam/university/java/core/task015/PointFactoryImpl.java
@@ -3,9 +3,6 @@ package com.epam.university.java.core.task015;
 public class PointFactoryImpl implements PointFactory {
     @Override
     public Point newInstance(double x, double y) {
-        Point point = new PointImpl();
-        point.setX(x);
-        point.setY(y);
-        return point;
+        return new PointImpl(x,y);
     }
 }
