fbeed9881945712ce5e67093c0d7df47e2056303
stelmakhmaksim <stelmakh96@mail.ru>
2017-10-06 12:56:42 +0300

    Tasks 28 & 30 are done. All tests are passed.

--- src/main/java/com/epam/university/java/core/task028/Task028Impl.java
+++ src/main/java/com/epam/university/java/core/task028/Task028Impl.java
@@ -1,4 +1,35 @@
 package com.epam.university.java.core.task028;
 
-public class Task028Impl {
+public class Task028Impl implements Task028 {
+    @Override
+    public int getWays(int value, int power) {
+        return checkRecursive(value, power, 1, 0);
+    }
+
+    private int checkRecursive(int value, int power, int curNum, int curSum) {
+        int results = 0;
+        //Calling power of 'curNum' raised to 'power'
+        int numPow = pow(curNum, power);
+        while (numPow + curSum < value) {
+            // Recursively check all greater values of 'curNum'
+            results += checkRecursive(value, power, curNum + 1, numPow + curSum);
+            curNum++;
+            numPow = pow(curNum, power);
+        }
+        if (numPow + curSum == value) {
+            results++;
+        }
+        return results;
+    }
+
+    // Calculates the power of given number
+    private int pow(int curNum, int pow) {
+        if (pow == 0) {
+            return 1;
+        } else if (pow % 2 == 0) {
+            return pow(curNum, pow / 2) * pow(curNum, pow / 2);
+        } else {
+            return curNum * pow(curNum, pow / 2) * pow(curNum, pow / 2);
+        }
+    }
 }
