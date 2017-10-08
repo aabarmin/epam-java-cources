94cb78d01e2e420616b2248073d267d413d75b4e
peter.fitenko@gmail.com <pd39583958>
2017-10-09 01:25:25 +0300

    optimised recursion task028

--- src/main/java/com/epam/university/java/core/task028/Task028Impl.java
+++ src/main/java/com/epam/university/java/core/task028/Task028Impl.java
@@ -2,29 +2,33 @@ package com.epam.university.java.core.task028;
 
 public class Task028Impl implements Task028 {
     private int result = 0;
-    
+
     @Override
     public int getWays(int value, int power) {
-        countWaysRecursive(value, value, 0, power);
-        return result;
-    }
-    
-    private void countWaysRecursive(int value, int diff, int tryNumber, int power) {
-        // if diff = 0 - we found another one solution
-        if (diff == 0) {
-            result++;
-        }
-        
-        // looping to the
+        // recursion should loop to the
         int countTo = (int) Math.floor(
                 Math.pow(value, 1.0 / power)
         );
+        
+        // count ways
+        countWaysRecursive(value, 0, power, countTo);
+        return result;
+    }
+
+    private void countWaysRecursive(int diff, int tryNumber, int power, int countTo) {
+        // if diff = 0 - we found another one solution
+        if (diff == 0) {
+            result++;
+            return;
+        }
 
         // recursive calling to find other numbers
-        for (int i = tryNumber + 1; i <= countTo; i++) {
+        for (int i = ++tryNumber; i <= countTo; i++) {
             int newDiff = diff - (int) Math.pow(i, power);
             if (newDiff >= 0) {
-                countWaysRecursive(value, newDiff, i, power);
+                countWaysRecursive(newDiff, i, power, countTo);
+            } else {
+                return;
             }
         }
     }
