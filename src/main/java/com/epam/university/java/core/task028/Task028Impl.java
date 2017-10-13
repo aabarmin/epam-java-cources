57daaaedc5ebbfd86c52cd3aedddfce0f003bb32
FruitAss <fruit9433@yandex.ru>
2017-10-14 00:58:52 +0200

    29-30

--- src/main/java/com/epam/university/java/core/task028/Task028Impl.java
+++ src/main/java/com/epam/university/java/core/task028/Task028Impl.java
@@ -1,9 +1,34 @@
 package com.epam.university.java.core.task028;
 
+import edu.princeton.cs.introcs.In;
+
+import java.util.HashSet;
+import java.util.concurrent.CopyOnWriteArraySet;
+
 public class Task028Impl implements Task028 {
     @Override
     public int getWays(int value, int power) {
-        int limit = (int) Math.pow(value, 1.0/power);
-        return 0;
+        return getWaysRecRoutine(0, value, 1, power, 0);
     }
+
+    private int getWaysRecRoutine(int sum, int target, int currentNum, int power, int ways) {
+        if (sum == target) {
+            return 1;
+        }
+        int localWays = ways;
+        for (int num = currentNum; sum + (int) Math.pow(num, power) <= target; ++num) {
+            localWays += getWaysRecRoutine(
+                    sum + (int) Math.pow(num, power),
+                    target,
+                    num + 1,
+                    power,
+                    ways
+            );
+        }
+        return localWays;
+    }
+
 }
+
+
+
---
+++ src/main/java/com/epam/university/java/core/task029/Indexes.java
@@ -0,0 +1,19 @@
+package com.epam.university.java.core.task029;
+
+public class Indexes {
+    private int x;
+    private int y;
+
+    public Indexes(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
