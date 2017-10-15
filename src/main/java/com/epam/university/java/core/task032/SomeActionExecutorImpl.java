396e7b2c7a47541ab11f77ed72112af9be89188e
a-sakhnenko <aksakhnenko@gmail.com>
2017-10-15 15:06:04 +0300

    Task032 is done

---
+++ src/main/java/com/epam/university/java/core/task032/SomeActionExecutorImpl.java
@@ -0,0 +1,13 @@
+package com.epam.university.java.core.task032;
+
+public class SomeActionExecutorImpl implements SomeActionExecutor {
+    @Override
+    public void doAction() {
+
+    }
+
+    @Override
+    public void doAnotherAction() {
+
+    }
+}
