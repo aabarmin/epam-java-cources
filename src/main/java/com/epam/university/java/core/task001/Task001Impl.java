15577dd07bb1847138d26907a5712b3a96025bbd
Paul Ponomarev <rocksquare@gmail.com>
2020-08-19 16:44:44 +0300

    Task001 done

---
+++ src/main/java/com/epam/university/java/core/task001/Task001Impl.java
@@ -0,0 +1,51 @@
+package com.epam.university.java.core.task001;
+
+public class Task001Impl implements Task001 {
+    @Override
+    public double addition(String firstNumber, String secondNumber) {
+        validateInput(firstNumber, secondNumber);
+
+        Integer first = Integer.parseInt(firstNumber.trim());
+        Integer second = Integer.parseInt(secondNumber.trim());
+
+        return first + second;
+    }
+
+    @Override
+    public double subtraction(String firstNumber, String secondNumber) {
+        validateInput(firstNumber, secondNumber);
+
+
+        Integer first = Integer.parseInt(firstNumber.trim());
+        Integer second = Integer.parseInt(secondNumber.trim());
+
+        return first - second;
+    }
+
+    @Override
+    public double multiplication(String firstNumber, String secondNumber) {
+        validateInput(firstNumber, secondNumber);
+
+        Double first = Double.parseDouble(firstNumber);
+        Double second = Double.parseDouble(secondNumber);
+
+        return first * second;
+    }
+
+    @Override
+    public double division(String firstNumber, String secondNumber) {
+        validateInput(firstNumber, secondNumber);
+
+
+        Double first = Double.parseDouble(firstNumber);
+        Double second = Double.parseDouble(secondNumber);
+
+        return first / second;
+    }
+
+    private void validateInput(String firstNumber, String secondNumber) {
+        if (firstNumber == null || secondNumber == null) {
+            throw new IllegalArgumentException();
+        }
+    }
+}
