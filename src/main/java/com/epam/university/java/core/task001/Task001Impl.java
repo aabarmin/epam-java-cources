a904b6dbbfc770046bf1fcb5bf85246f684496c2
Pavel <gunpavel.i@gmail.com>
2020-08-19 10:59:33 +0300

    Added h1 tag

---
+++ src/main/java/com/epam/university/java/core/task001/Task001Impl.java
@@ -0,0 +1,65 @@
+package com.epam.university.java.core.task001;
+
+public class Task001Impl implements Task001 {
+
+
+    /**
+     * Execute addition operation.
+     *
+     * @param firstNumber  string value of first number
+     * @param secondNumber string value of second number
+     * @return result of addition operation
+     * @throws IllegalArgumentException if input parameters are not set
+     * @throws NumberFormatException    if can't convert input values to numbers
+     */
+    @Override
+    public double addition(String firstNumber, String secondNumber) {
+        if (firstNumber == null || secondNumber == null || firstNumber.equals("") || secondNumber.equals("")) throw new IllegalArgumentException();
+        return Double.parseDouble(firstNumber) + Double.parseDouble(secondNumber);
+    }
+
+    /**
+     * Execute subtraction operation.
+     *
+     * @param firstNumber  string value of first number
+     * @param secondNumber string value of second number
+     * @return result of subtraction operation
+     * @throws IllegalArgumentException if input parameters are not set
+     * @throws NumberFormatException    if can't convert input values to numbers
+     */
+    @Override
+    public double subtraction(String firstNumber, String secondNumber) {
+        if (firstNumber == null || secondNumber == null || firstNumber.equals("") || secondNumber.equals("")) throw new IllegalArgumentException();
+        return Double.parseDouble(firstNumber) - Double.parseDouble(secondNumber);
+    }
+
+    /**
+     * Execute multiplication operation.
+     *
+     * @param firstNumber  string value of first number
+     * @param secondNumber string value of second number
+     * @return result of multiplication operation
+     * @throws IllegalArgumentException if input parameters are not set
+     * @throws NumberFormatException    if can't convert input values to numbers
+     */
+    @Override
+    public double multiplication(String firstNumber, String secondNumber) {
+        if (firstNumber == null || secondNumber == null || firstNumber.equals("") || secondNumber.equals("")) throw new IllegalArgumentException();
+        return Double.parseDouble(firstNumber) * Double.parseDouble(secondNumber);
+    }
+
+    /**
+     * Execute division operation.
+     *
+     * @param firstNumber  string value of first number
+     * @param secondNumber string value of second number
+     * @return result of division operation
+     * @throws IllegalArgumentException if input parameters are not set
+     * @throws NumberFormatException    if can't convert input values to numbers
+     */
+    @Override
+    public double division(String firstNumber, String secondNumber) {
+        if (firstNumber == null || secondNumber == null || firstNumber.equals("") || secondNumber.equals("")) throw new IllegalArgumentException();
+        return Double.parseDouble(firstNumber) / Double.parseDouble(secondNumber);
+    }
+}
