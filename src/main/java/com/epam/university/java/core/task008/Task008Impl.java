1c55e2d4d2244757c86127edbf3b691dc7305614
arcanrun <arcan.run@gmail.com>
2020-08-22 19:10:50 +0300

    task08: empyt stakc exception

---
+++ src/main/java/com/epam/university/java/core/task008/Task008Impl.java
@@ -0,0 +1,41 @@
+package com.epam.university.java.core.task008;
+
+import java.util.Stack;
+
+public class Task008Impl implements Task008 {
+    @Override
+    public boolean isValid(String sourceString) {
+        String braces = "[]{}()";
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < sourceString.length(); i++) {
+            char c = sourceString.charAt(i);
+            if (c == '{' || c == '(' || c == '[') {
+                stack.push(c);
+            }
+            if (c == '}') {
+                if (stack.peek() != '{') {
+                    return false;
+                } else {
+                    stack.pop();
+                }
+            }
+
+            if (c == ')') {
+                if (stack.peek() != '(') {
+                    return false;
+                } else {
+                    stack.pop();
+                }
+            }
+
+            if (c == ']') {
+                if (stack.peek() != '[') {
+                    return false;
+                } else {
+                    stack.pop();
+                }
+            }
+        }
+        return stack.size() == 0;
+    }
+}
