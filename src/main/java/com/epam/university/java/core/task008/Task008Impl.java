de596455430c4c6854ac9ef34fe65650c091a2d6
Petr Telepaev <Petrtelepaev@yandex.ru>
2020-08-22 22:55:55 +0300

    task 8 is solved

---
+++ src/main/java/com/epam/university/java/core/task008/Task008Impl.java
@@ -0,0 +1,53 @@
+package com.epam.university.java.core.task008;
+
+import java.util.ArrayList;
+
+public class Task008Impl implements Task008 {
+    @Override
+    public boolean isValid(String sourceString) {
+
+        if (sourceString.isEmpty()) {
+            return true;
+        }
+        char[] chars = sourceString.toCharArray();
+        ArrayList<Character> list = new ArrayList<>();
+
+        for (int i = 0; i < chars.length; i++) {
+            if (chars[i] == '(' || chars[i] == '{' || chars[i] == '[') {
+                list.add(chars[i]);
+            }
+            if (chars[i] == ')' || chars[i] == '}' || chars[i] == ']') {
+                if (list.size() == 0) {
+                    return false;
+                }
+                switch (chars[i]) {
+                    case ')':
+                        if (!(list.get(list.size() - 1) == '(')) {
+                            return false;
+                        } else {
+                            list.remove(list.size() - 1);
+                        }
+                        break;
+                    case '}':
+                        if (!(list.get(list.size() - 1) == '{')) {
+                            return false;
+                        } else {
+                            list.remove(list.size() - 1);
+                        }
+                        break;
+                    case ']':
+                        if (!(list.get(list.size() - 1) == '[')) {
+                            return false;
+                        } else {
+                            list.remove(list.size() - 1);
+                        }
+                        break;
+                }
+            }
+        }
+        if (list.size() != 0) {
+            return false;
+        }
+        return true;
+    }
+}
