67c61a57530c953433fa69f803f5445c6d4a32f9
Petr Telepaev <Petrtelepaev@yandex.ru>
2020-08-22 10:02:36 +0300

    I have some questions about task#5, byt task #7 is solved

---
+++ src/main/java/com/epam/university/java/core/task005/Task005Impl.java
@@ -0,0 +1,55 @@
+package com.epam.university.java.core.task005;
+
+public class Task005Impl implements Task005 {
+
+    @Override
+    public PiHolder findPi(int digits) {
+
+        if (digits <= 0 || digits > 10) {
+            throw new IllegalArgumentException();
+        }
+        int min = (int) Math.pow(10, digits - 1);
+        int max = (int) Math.pow(10, digits) - 1;
+        int first = min;
+        int second = min;
+        int fOut = min;
+        int sOut = min;
+
+        while (first < max) {
+            while (second < max) {
+                if ((Math.abs((float) first / second -Math.PI)) < (Math.abs((float)fOut / sOut - Math.PI))) {
+                    fOut = first;
+                    sOut = second;
+                }
+                second++;
+            }
+            first++;
+            second = min;
+        }
+/*
+        switch (digits) {
+            case 1:
+                first = 3;
+                second = 1;
+                break;
+            case 2:
+                first = 44;
+                second = 14;
+                break;
+            case 3:
+                first = 355;
+                second = 113;
+                break;
+            case 4:
+                first = 99773;
+                second = 31746;
+                break;
+            case 5:
+                first = 99733;
+                second = 31746;
+            case 6:
+
+        }*/
+        return new PiHolderImpl(fOut, sOut);
+    }
+}
---
+++ src/main/java/com/epam/university/java/core/task007/Task007Impl.java
@@ -0,0 +1,27 @@
+package com.epam.university.java.core.task007;
+
+import java.util.*;
+
+public class Task007Impl implements Task007 {
+    @Override
+    public Collection<Integer> multiplyPolynomial(Collection<Integer> first, Collection<Integer> second) {
+
+        ArrayList<Integer> fPol = new ArrayList<>(first);
+        ArrayList<Integer> sPol = new ArrayList<>(second);
+
+        int[] prod = new int[fPol.size() + sPol.size() - 1];
+
+        for (int i = 0; i < fPol.size(); i++) {
+            for (int j = 0; j < sPol.size(); j++) {
+                prod[i + j] += fPol.get(i) * sPol.get(j);
+            }
+        }
+
+        ArrayList<Integer> list = new ArrayList<>();
+        for (Integer num : prod) {
+            list.add(num);
+        }
+
+        return list;
+    }
+}
