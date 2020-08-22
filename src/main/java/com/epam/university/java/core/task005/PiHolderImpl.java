01e5677a1081fcaca3b0c6ea256ef1f504c20050
arcanrun <arcan.run@gmail.com>
2020-08-22 16:10:26 +0300

    task05: init

---
+++ src/main/java/com/epam/university/java/core/task005/PiHolderImpl.java
@@ -0,0 +1,24 @@
+package com.epam.university.java.core.task005;
+
+public class PiHolderImpl implements PiHolder{
+    private int first;
+    private int second;
+
+    public void setFirst(int first) {
+        this.first = first;
+    }
+
+    public void setSecond(int second) {
+        this.second = second;
+    }
+
+    @Override
+    public int getFirst() {
+        return 0;
+    }
+
+    @Override
+    public int getSecond() {
+        return 0;
+    }
+}
---
+++ src/main/java/com/epam/university/java/core/task005/Task005Impl.java
@@ -0,0 +1,9 @@
+package com.epam.university.java.core.task005;
+
+public class Task005Impl implements Task005{
+    @Override
+    public PiHolder findPi(int digits) {
+//        int a = Math.pow(10, digits-1);
+        return null;
+    }
+}
