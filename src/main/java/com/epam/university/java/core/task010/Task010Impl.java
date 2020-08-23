3511835d3148463e149416546645c30da645b9e2
ZzOneDay <azminseo@gmail.com>
2020-08-23 22:08:21 +0300

    Solution010

---
+++ src/main/java/com/epam/university/java/core/task010/Task010Impl.java
@@ -0,0 +1,38 @@
+package com.epam.university.java.core.task010;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Task010Impl implements Task010 {
+    @Override
+    public Map<String, Integer> countWordNumbers(File source) {
+        String text = "";
+        try {
+            Scanner scanner = new Scanner(source);
+            while (scanner.hasNext()) {
+                text += scanner.nextLine();
+            }
+            ;
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        text = text.replaceAll("[\\.,]", "");
+        String[] words = text.split("\\s+");
+
+        HashMap<String, Integer> mapOfWords = new HashMap<>();
+        for (String word : words) {
+            if (mapOfWords.containsKey(word)) {
+                int count = mapOfWords.get(word);
+                mapOfWords.put(word, ++count);
+            } else {
+                int count = 1;
+                mapOfWords.put(word.toLowerCase(), count);
+            }
+        }
+        return mapOfWords;
+    }
+}
