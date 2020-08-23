046c21d66d14e46f143d3c915329853f58e85bd1
ZzOneDay <azminseo@gmail.com>
2020-08-23 04:03:13 +0300

    Solution005

---
+++ src/main/java/com/epam/university/java/core/task005/Task005Impl.java
@@ -0,0 +1,37 @@
+package com.epam.university.java.core.task005;
+
+public class Task005Impl implements Task005 {
+    @Override
+    public PiHolder findPi(int digits) {
+        if (digits == 0 || digits > 10) {
+            throw new IllegalArgumentException();
+        }
+
+        String numberLow = "1";
+        String numberMax = "9";
+        for (int i = 0; i < digits - 1; i++) {
+            numberLow += 0;
+            numberMax += 9;
+        }
+
+        int low = Integer.parseInt(numberLow);
+        int maxForFirst = Integer.parseInt(numberMax);;
+        int maxForSecond = maxForFirst / 3;
+
+        int firstValue = 1;
+        int secondValue = 1;
+        double resultValue = 9;
+
+        for (int first = low; first < maxForFirst; first++) {
+            for (int second = low; second < maxForSecond; second++) {
+                double value = Math.abs(((double) first / second) - Math.PI);
+                if (resultValue > value) {
+                    resultValue = value;
+                    firstValue = first;
+                    secondValue = second;
+                }
+            }
+        }
+        return new PiHolderImpl(firstValue, secondValue);
+    }
+}
