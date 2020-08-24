140cede9f53a1ae3ada832c878f5f97fb0b8b820
Romin <romin.nuro@gmail.com>
2020-08-25 00:46:04 +0300

    Task 012 done

---
+++ src/main/java/com/epam/university/java/core/task012/GraphFactoryImpl.java
@@ -0,0 +1,21 @@
+package com.epam.university.java.core.task012;
+
+/**
+ * Created by Romin Nuro on 24.08.2020 20:44.
+ */
+public class GraphFactoryImpl implements GraphFactory {
+    /**
+     * Create new Graph instance with designated amount of vertexes.
+     *
+     * @param vertexesCount vertexes count
+     * @return new graph instance
+     */
+    @Override
+    public Graph newInstance(int vertexesCount) {
+        GraphImpl graph = new GraphImpl();
+        for (int i = 1; i <= vertexesCount; i++) {
+            graph.createVertex(i);
+        }
+        return graph;
+    }
+}
