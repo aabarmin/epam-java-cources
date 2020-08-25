3aff0d73b01a6fc38985d787754022499beaa71a
Paul Ponomarev <rocksquare@gmail.com>
2020-08-25 18:37:12 +0300

    task013 done!

---
+++ src/main/java/com/epam/university/java/core/task013/FigureFactoryImpl.java
@@ -0,0 +1,13 @@
+package com.epam.university.java.core.task013;
+
+public class FigureFactoryImpl implements FigureFactory {
+    @Override
+    public Figure newInstance(int vertexCount) {
+        return null;
+    }
+
+    @Override
+    public Vertex newInstance(int x, int y) {
+        return null;
+    }
+}
