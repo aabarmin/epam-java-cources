7330bdf435e4b08a4545d15951a8113310c46c0c
Romin <romin.nuro@gmail.com>
2020-08-26 11:32:32 +0300

    Task013 first part

---
+++ src/main/java/com/epam/university/java/core/task013/FigureImpl.java
@@ -0,0 +1,43 @@
+package com.epam.university.java.core.task013;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Created by Romin Nuro on 26.08.2020 0:04.
+ */
+public class FigureImpl implements Figure {
+    private final List<Vertex> vertexes;
+    private final int vertexCount;
+
+    public FigureImpl(int vertexCount) {
+        vertexes = new ArrayList<>();
+        this.vertexCount = vertexCount;
+    }
+
+    /**
+     * Add vertex to figure with designated coordinates.
+     *
+     * @param vertex vertex to add
+     */
+    @Override
+    public void addVertex(Vertex vertex) {
+        if (vertexes.size() < vertexCount) {
+            vertexes.add(vertex);
+        } else {
+            System.err.println("Can't add vertex, all vertexes are set");
+        }
+    }
+
+    /**
+     * Get all vertexes of figure.
+     *
+     * @return collection of vertexes
+     */
+    @Override
+    public Collection<Vertex> getVertexes() {
+        return vertexes;
+    }
+}
