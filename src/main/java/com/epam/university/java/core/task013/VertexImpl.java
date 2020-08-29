e68b808a10e2de7e64052e53c8997089fd6c69c9
svetlana.perekrestova <svetlana.perekrestova@syngenta.com>
2020-08-29 14:41:04 +0300

    Task013 is resolved

---
+++ src/main/java/com/epam/university/java/core/task013/VertexImpl.java
@@ -0,0 +1,31 @@
+package com.epam.university.java.core.task013;
+
+public class VertexImpl implements Vertex {
+    private int x;
+    private int y;
+
+    public VertexImpl(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public int getX() {
+        return x;
+    }
+
+    @Override
+    public void setX(int value) {
+        this.x = value;
+    }
+
+    @Override
+    public int getY() {
+        return y;
+    }
+
+    @Override
+    public void setY(int value) {
+        this.y = value;
+    }
+}
