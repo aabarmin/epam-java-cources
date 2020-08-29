08433f11cf088ae98e3bec0db70f1bb60a7b0cbc
Anton Kim <a.a.kim3101@gmail.com>
2020-08-29 20:53:27 +0300

    Implement Task001

---
+++ src/main/java/com/epam/university/java/core/task001/Task001Impl.java
@@ -0,0 +1,50 @@
+package com.epam.university.java.core.task001;
+
+public class Task001Impl implements Task001{
+
+    @Override
+    public double addition(String firstNumber, String secondNumber) throws IllegalArgumentException{
+        if (firstNumber == null || secondNumber == null){
+            throw new IllegalArgumentException();
+        }
+        double firstNumberParsed = Double.parseDouble(firstNumber);
+        double secondNumberParsed = Double.parseDouble(secondNumber);
+        return firstNumberParsed + secondNumberParsed;
+    }
+
+    @Override
+    public double subtraction(String firstNumber, String secondNumber) throws IllegalArgumentException{
+        if (firstNumber == null || secondNumber == null){
+            throw new IllegalArgumentException();
+        }
+        double firstNumberParsed = Double.parseDouble(firstNumber);
+        double secondNumberParsed = Double.parseDouble(secondNumber);
+        return firstNumberParsed - secondNumberParsed;
+    }
+
+    @Override
+    public double multiplication(String firstNumber, String secondNumber) throws IllegalArgumentException{
+        if (firstNumber == null || secondNumber == null){
+            throw new IllegalArgumentException();
+        }
+        double firstNumberParsed = Double.parseDouble(firstNumber);
+        double secondNumberParsed = Double.parseDouble(secondNumber);
+        return firstNumberParsed * secondNumberParsed;
+    }
+
+    @Override
+    public double division(String firstNumber, String secondNumber) throws IllegalArgumentException{
+        if (firstNumber == null || secondNumber == null){
+            throw new IllegalArgumentException();
+        }
+        double firstNumberParsed = Double.parseDouble(firstNumber);
+        double secondNumberParsed = Double.parseDouble(secondNumber);
+//        if (secondNumberParsed == 0){
+//            System.out.println("Incorrect division results");
+//            return 0;
+//        }
+//        else {
+            return firstNumberParsed / secondNumberParsed;
+//        }
+    }
+}
