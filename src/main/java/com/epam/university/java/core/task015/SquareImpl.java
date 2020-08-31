6cd1bc3014abe241b71709f1f6b6b4ea438e2834
Svetlana Perekrestova <Sherlock.Nacht@gmail.com>
2020-08-31 10:47:02 +0300

    half of Task015

---
+++ src/main/java/com/epam/university/java/core/task015/SquareImpl.java
@@ -0,0 +1,32 @@
+package com.epam.university.java.core.task015;
+
+public class SquareImpl implements Square {
+
+    private Point first;
+    private Point second;
+
+    public SquareImpl(Point first, Point second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    @Override
+    public Point getFirst() {
+        return first;
+    }
+
+    @Override
+    public Point getSecond() {
+        return second;
+    }
+
+    @Override
+    public void setFirst(Point first) {
+        this.first = first;
+    }
+
+    @Override
+    public void setSecond(Point second) {
+        this.second = second;
+    }
+}
