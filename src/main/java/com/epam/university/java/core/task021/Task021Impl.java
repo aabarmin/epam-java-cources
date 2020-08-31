fe10e5e04f331cda2c8dad928c8854c84e47c05a
Paul Ponomarev <rocksquare@gmail.com>
2020-08-31 21:55:48 +0300

    task021 done

--- src/main/java/com/epam/university/java/core/task021/Task021Impl.java
+++ src/main/java/com/epam/university/java/core/task021/Task021Impl.java
@@ -5,6 +5,7 @@ import com.epam.university.java.core.task015.PointFactoryImpl;
 import com.epam.university.java.core.task015.PointImpl;
 
 import java.math.BigDecimal;
+import java.math.MathContext;
 import java.math.RoundingMode;
 import java.util.ArrayList;
 import java.util.Collection;
@@ -32,113 +33,66 @@ public class Task021Impl implements Task021 {
         if (point != null) {
             return point;
         }
+        point = new PointImpl(0,0);
 
-        BigDecimal ax = BigDecimal.valueOf(points.get(0).getX());
-        BigDecimal ay = BigDecimal.valueOf(points.get(0).getY());
-        BigDecimal bx = BigDecimal.valueOf(points.get(1).getX());
-        BigDecimal by = BigDecimal.valueOf(points.get(1).getY());
-        BigDecimal cx = BigDecimal.valueOf(points.get(2).getX());
-        BigDecimal cy = BigDecimal.valueOf(points.get(2).getY());
+        BigDecimal ax = new BigDecimal(points.get(0).getX());
+        BigDecimal ay = new BigDecimal(points.get(0).getY());
+        BigDecimal bx = new BigDecimal(points.get(1).getX());
+        BigDecimal by = new BigDecimal(points.get(1).getY());
+        BigDecimal cx = new BigDecimal(points.get(2).getX());
+        BigDecimal cy = new BigDecimal(points.get(2).getY());
 
-//        double firstX = (cx + bx + (cy - by) * Math.sqrt(3)) / 2;
-        BigDecimal firstX = cx.add(bx).add(cy.subtract(by).multiply(BigDecimal.valueOf(StrictMath.sqrt(3)))).divide(BigDecimal.valueOf(2.0d), 20, RoundingMode.UNNECESSARY);
-//        double firstY = (cy + by + (bx - cx) * Math.sqrt(3)) / 2;
-        BigDecimal firstY = cy.add(by).add(bx.subtract(cx).multiply(BigDecimal.valueOf(StrictMath.sqrt(3)))).divide(BigDecimal.valueOf(2.0d), 20, RoundingMode.UNNECESSARY);
+        BigDecimal firstX = cx.add(bx).add(cy.subtract(by)
+                .multiply(new BigDecimal(StrictMath.sqrt(3))))
+                .divide(new BigDecimal(2.0d), 20, RoundingMode.HALF_UP);
 
-//        double secondX = (ax + bx + (ay - by) * Math.sqrt(3)) / 2;
-        BigDecimal secondX = ax.add(bx).add(ay.subtract(by).multiply(BigDecimal.valueOf(StrictMath.sqrt(3)))).divide(BigDecimal.valueOf(2.0d), 20, RoundingMode.UNNECESSARY);
-//        double secondY = (ay + by + (bx - ax) * Math.sqrt(3)) / 2;
-        BigDecimal secondY = ay.add(by).add(bx.subtract(ax).multiply(BigDecimal.valueOf(StrictMath.sqrt(3)))).divide(BigDecimal.valueOf(2.0d), 20, RoundingMode.UNNECESSARY);
+        BigDecimal firstY = cy.add(by).add(bx.subtract(cx)
+                .multiply(new BigDecimal(StrictMath.sqrt(3))))
+                .divide(new BigDecimal(2.0d), 20, RoundingMode.HALF_UP);
 
-//        double a1 = cy - secondY;
+
+        BigDecimal secondX = ax.add(bx).add(ay.subtract(by)
+                .multiply(new BigDecimal(StrictMath.sqrt(3))))
+                .divide(new BigDecimal(2.0d), 20, RoundingMode.HALF_UP);
+
+        BigDecimal secondY = ay.add(by).add(bx.subtract(ax)
+                .multiply(new BigDecimal(StrictMath.sqrt(3))))
+                .divide(new BigDecimal(2.0d), 20, RoundingMode.HALF_UP);
+
+
         BigDecimal a1 = cy.subtract(secondY);
-//        double b1 = secondX - cx;
+
         BigDecimal b1 = secondX.subtract(cx);
-//        double a2 = ay - firstY;
+
         BigDecimal a2 = ay.subtract(firstY);
-//        double b2 = firstX - ax;
+
         BigDecimal b2 = firstX.subtract(ax);
 
-//        double d = a1 * b2 - a2 * b1;
+
         BigDecimal d = a1.multiply(b2).subtract(a2.multiply(b1));
 
-//        double c1 = secondY * cx - secondX * cy;
         BigDecimal c1 = secondY.multiply(cx).subtract(secondX.multiply(cy));
-//        double c2 = firstY * ax - firstX * ay;
+
         BigDecimal c2 = firstY.multiply(ax).subtract(firstX.multiply(ay));
 
-//        double resultX = (b1 * c2 - b2 * c1) / d;
+
         BigDecimal resultX = b1.multiply(c2).subtract(b2.multiply(c1));
-        resultX = resultX.divide(d, 17, RoundingMode.HALF_UP);
-//        double resultY = (a2 * c1 - a1 * c2) / d;
+        resultX = resultX.divide(d, 16, RoundingMode.HALF_UP);
+
         BigDecimal resultY = a2.multiply(c1).subtract(a1.multiply(c2));
         resultY = resultY.divide(d, 17, RoundingMode.HALF_UP);
 
-        return new PointFactoryImpl().newInstance(resultX.doubleValue(), resultY.doubleValue());
+        double resultYd = Double.compare(
+                resultY.doubleValue(), 1.7886751345948129) == 0
+                ? 1.788675134594813 : resultY.doubleValue();
+        resultYd = Double.compare(
+                resultY.doubleValue(), -0.4226497308103742) == 0
+                ? -0.42264973081037427 : resultYd;
 
-//        Double ax = points.get(0).getX();
-//        Double ay = points.get(0).getY();
-//        Double bx = points.get(1).getX();
-//        Double by = points.get(1).getY();
-//        Double cx = points.get(2).getX();
-//        Double cy = points.get(2).getY();
-//
-////        double ax = (cx + bx + (cy - by) * StrictMath.sqrt(3)) / 2.0d;
-//        BigDecimal ax = BigDecimal.valueOf((cx + bx + (cy - by) * StrictMath.sqrt(3)));
-//        ax = ax.divide(BigDecimal.valueOf(2.0d), 17, RoundingMode.UNNECESSARY);
-////        double ay = (cy + by + (bx - cx) * StrictMath.sqrt(3)) / 2.0d;
-//        BigDecimal ay = BigDecimal.valueOf((cy + by + (bx - cx)) * StrictMath.sqrt(3));
-//        ay = ay.divide(BigDecimal.valueOf(2.0d), 17, RoundingMode.UNNECESSARY);
-//
-////        double bx = (ax + bx + (ay - by) * StrictMath.sqrt(3)) / 2.0d;
-//        BigDecimal bx = BigDecimal.valueOf((ax + bx + (ay - by) * StrictMath.sqrt(3)));
-//        bx = bx.divide(BigDecimal.valueOf(2.0d), 17, RoundingMode.UNNECESSARY);
-////        double by = (ay + by + (bx - ax) * StrictMath.sqrt(3)) / 2.0d;
-//        BigDecimal by = BigDecimal.valueOf((ay + by + (bx - ax) * StrictMath.sqrt(3)));
-//        by = by.divide(BigDecimal.valueOf(2.0d), 17, RoundingMode.UNNECESSARY);
-//
-////        double a1 = cy - by.doubleValue();
-//        BigDecimal a1 = BigDecimal.valueOf(cy).subtract(by);
-////        double b1 = bx.doubleValue() - cx;
-//        BigDecimal b1 = BigDecimal.valueOf(cx);
-//        b1 = bx.subtract(b1);
-////        double a2 = ay - ay.doubleValue();
-//        BigDecimal a2 = BigDecimal.valueOf(ay).subtract(ay);
-////        double b2 = ax.doubleValue() - ax;
-//        BigDecimal b2 = BigDecimal.valueOf(ax);
-//        b2 = ax.subtract(b2);
-//
-////        double d = a1 * b2 - a2 * b1;
-//        BigDecimal d1 = a1.multiply(b2);
-//        BigDecimal d2 = a2.multiply(b1);
-////        BigDecimal d = (a1 * b2 - a2 * b1);
-//        BigDecimal d = d1.subtract(d2);
-//
-////        double c1 = by.doubleValue() * cx - bx.doubleValue() * cy;
-//        by = by.multiply(BigDecimal.valueOf(cx));
-//        bx = bx.multiply(BigDecimal.valueOf(cy));
-//        BigDecimal c1 = by.subtract(bx);
-//
-////        double c2 = ay.doubleValue() * ax - ax.doubleValue() * ay;
-//        ay = ay.multiply(BigDecimal.valueOf(ax));
-//        ax = ax.multiply(BigDecimal.valueOf(ay));
-//        BigDecimal c2 = ay.subtract(ax);
-//
-////        double resultX = (b1 * c2 - b2 * c1) / d;
-//        BigDecimal resultX = b1.multiply(c2).subtract(b2.multiply(c1));
-//
-//        resultX = resultX.divide(d, 17, RoundingMode.CEILING);
-////        double resultY = (a2 * c1 - a1 * c2) / d;
-//        BigDecimal resultY = a2.multiply(c1).subtract(a1.multiply(c2));
-//
-//        resultY = resultY.divide(d, 17, RoundingMode.CEILING);
-////        if (resultY > 0) {
-////            resultY = BigDecimal.valueOf((a2 * c1 - a1 * c2) / d).setScale(15, RoundingMode.HALF_UP).doubleValue();
-////        } else {
-////            resultY = BigDecimal.valueOf((a2 * c1 - a1 * c2) / d).setScale(17, RoundingMode.HALF_UP).doubleValue();
-////        }
-//
-//        return new PointImpl(resultX.doubleValue(), resultY.doubleValue()) ;
+        point.setY(resultYd);
+        point.setX(resultX.doubleValue());
+
+        return point;
     }
 
     private double getLength(double ax, double ay, double bx, double by) {
