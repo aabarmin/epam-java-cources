b5ca0171a412fb05bc223b0f65b256190896178e
svetlana.perekrestova <svetlana.perekrestova@syngenta.com>
2020-09-01 21:13:41 +0300

    Task018 is resolved + checkStyle updated

--- src/main/java/com/epam/university/java/core/task018/Task018Impl.java
+++ src/main/java/com/epam/university/java/core/task018/Task018Impl.java
@@ -1,6 +1,9 @@
 package com.epam.university.java.core.task018;
 
 import java.lang.annotation.Annotation;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 
 public class Task018Impl implements Task018 {
     @Override
@@ -8,12 +11,47 @@ public class Task018Impl implements Task018 {
         if (toCheck == null || annotationToFind == null) {
             throw new IllegalArgumentException();
         }
-        Annotation[] annotations = toCheck.getClass().getAnnotations();
-        Annotation[] annotations2 = annotationToFind.getAnnotations();
-        if (annotationToFind.isInstance(BasicAnnotation.class)) {
-
-        } else {
-            // for package
+        Class clazz = toCheck.getClass();
+        if (clazz.isAnnotationPresent(annotationToFind)) {
+            return true;
+        }
+        Constructor<?>[] constructors = clazz.getDeclaredConstructors();
+        for (Constructor<?> con : constructors) {
+            for (Annotation an : con.getDeclaredAnnotations()) {
+                if (an.annotationType().equals(annotationToFind)) {
+                    return true;
+                }
+            }
+        }
+        Method[] methods = clazz.getDeclaredMethods();
+        for (Method method : methods) {
+            for (Annotation an : method.getDeclaredAnnotations()) {
+                if (an.annotationType().equals(annotationToFind)) {
+                    return true;
+                }
+            }
+            Annotation[][] annotations = method.getParameterAnnotations();
+            for (Annotation[] an : annotations) {
+                for (Annotation annotation : an) {
+                    if (annotation.annotationType().equals(annotationToFind)) {
+                        return true;
+                    }
+                }
+            }
+        }
+        Field[] fields = clazz.getDeclaredFields();
+        for (Field f : fields) {
+            for (Annotation an : f.getDeclaredAnnotations()) {
+                if (an.annotationType().equals(annotationToFind)) {
+                    return true;
+                }
+            }
+        }
+        Package pack = clazz.getPackage();
+        for (Annotation an : pack.getAnnotations()) {
+            if (an.annotationType().equals(annotationToFind)) {
+                return true;
+            }
         }
         return false;
     }
