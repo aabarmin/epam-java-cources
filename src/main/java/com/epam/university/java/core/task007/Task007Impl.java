568812c384a8918a4a77cd844cec95a8eb971a42
Svetlana Perekrestova <Sherlock.Nacht@gmail.com>
2020-09-02 21:47:58 +0300

    Bugfixes

--- src/main/java/com/epam/university/java/core/task007/Task007Impl.java
+++ src/main/java/com/epam/university/java/core/task007/Task007Impl.java
@@ -10,7 +10,7 @@ public class Task007Impl implements Task007 {
     @Override
     public Collection<Integer> multiplyPolynomial(Collection<Integer> first,
                                                   Collection<Integer> second) {
-        if (first == null && second == null) {
+        if (first == null || second == null) {
             throw new IllegalArgumentException();
         }
         double[] firstDouble = new double[first.size()];
