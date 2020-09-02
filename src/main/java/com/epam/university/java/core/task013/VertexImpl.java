d42b47eab9809554cd80b2368128f3d050e0012c
Dmitry Novikov <novicovdm@gmail.com>
2020-09-02 20:00:17 +0300

    Task 13 temp

---
+++ src/main/java/com/epam/university/java/core/task013/VertexImpl.java
@@ -0,0 +1,34 @@
+package com.epam.university.java.core.task013;
+
+/**
+ * @author Dmitry Novikov 02-Sep-20
+ */
+public class VertexImpl implements Vertex {
+    private int x;
+    private  int y;
+
+    public VertexImpl(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public int getX() {
+        return x;
+    }
+
+    @Override
+    public void setX(int value) {
+        x = value;
+    }
+
+    @Override
+    public int getY() {
+        return y;
+    }
+
+    @Override
+    public void setY(int value) {
+        y = value;
+    }
+}
