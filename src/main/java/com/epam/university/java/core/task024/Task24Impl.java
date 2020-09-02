9b0578d53faa1e0192cd6e2e6167d76cd9aa78f7
Dmitry Novikov <novicovdm@gmail.com>
2020-09-02 20:18:31 +0300

    Task 24 solved

---
+++ src/main/java/com/epam/university/java/core/task024/Task24Impl.java
@@ -0,0 +1,30 @@
+package com.epam.university.java.core.task024;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * author Dmitry Novikov 02-Sep-20.
+ */
+public class Task24Impl implements Task24 {
+    @Override
+    public Collection<String> getWordsCount(String source) {
+        List<String> result = new ArrayList<>();
+
+        int startIndex = 0;
+        int endIndex = 0;
+
+        for (int i = 0; i < source.length(); i++) {
+            if (Character.isUpperCase(source.charAt(i)) || i == source.length() - 1) {
+                startIndex = endIndex;
+                endIndex = i != source.length() - 1 ? i : i + 1;
+                String temp = source.substring(startIndex, endIndex).toLowerCase();
+                if (!temp.isEmpty()) {
+                    result.add(temp);
+                }
+            }
+        }
+        return result;
+    }
+}
