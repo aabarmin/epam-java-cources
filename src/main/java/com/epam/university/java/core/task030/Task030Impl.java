dd214ae6e036f7723910e7dc9c23f28a9fbae6bc
Svetlana Perekrestova <Sherlock.Nacht@gmail.com>
2020-09-05 23:46:20 +0300

    Task030 is resolved

---
+++ src/main/java/com/epam/university/java/core/task030/Task030Impl.java
@@ -0,0 +1,45 @@
+package com.epam.university.java.core.task030;
+
+import java.time.DayOfWeek;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.TemporalAdjusters;
+
+import static java.time.temporal.ChronoUnit.DAYS;
+
+public class Task030Impl implements Task030 {
+    @Override
+    public int daysBetweenDates(LocalDate dateStart, LocalDate dateEnd) {
+        return (int) DAYS.between(dateStart, dateEnd);
+    }
+
+    @Override
+    public LocalDate adjustDays(LocalDate dateStart, int daysToAdd) {
+        return dateStart.plusDays(daysToAdd);
+    }
+
+    @Override
+    public long distanceBetween(Instant instantStart, Instant instantEnd) {
+        Duration between = Duration.between(instantStart, instantEnd);
+        return between.getSeconds();
+    }
+
+    @Override
+    public DayOfWeek getDayOfWeek(LocalDate localDate) {
+        return localDate.getDayOfWeek();
+    }
+
+    @Override
+    public LocalDate getNextWeekend(LocalDate localDate) {
+        return localDate.with(TemporalAdjusters.next(DayOfWeek.SATURDAY));
+    }
+
+    @Override
+    public LocalTime getLocalTime(String timeString) {
+
+        return LocalTime.parse(timeString, DateTimeFormatter.ofPattern("hh:mma"));
+    }
+}
