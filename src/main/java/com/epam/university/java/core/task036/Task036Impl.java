5f98f1c140de3d5540eb9a6bc292fd511f01f686
Paul Ponomarev <rocksquare@gmail.com>
2020-09-07 15:04:19 +0300

    task037 done + checkstyle updates

--- src/main/java/com/epam/university/java/core/task036/Task036Impl.java
+++ src/main/java/com/epam/university/java/core/task036/Task036Impl.java
@@ -4,7 +4,10 @@ import java.util.function.Function;
 
 public class Task036Impl implements Task036 {
     @Override
-    public double integrate(Function<Double, Double> function, Integrator integrator, double limitLeft, double limitRight) {
+    public double integrate(Function<Double, Double> function,
+                            Integrator integrator,
+                            double limitLeft,
+                            double limitRight) {
         return integrator.integrate(limitLeft, limitRight, function);
     }
 }
