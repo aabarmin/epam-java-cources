82c542f80f96cd4db3ed6fcdb30511b0d37d8470
Romin <romin.nuro@gmail.com>
2020-09-08 15:16:26 +0300

    Task026 started

---
+++ src/main/java/com/epam/university/java/core/task026/Task026Impl.java
@@ -0,0 +1,30 @@
+package com.epam.university.java.core.task026;
+
+/**
+ * Created by Romin Nuro on 08.09.2020 15:16.
+ */
+public class Task026Impl implements Task026 {
+    /**
+     * Encrypt source string with Caesar Cipher shifting by <code>shift</code> value.
+     *
+     * @param sourceString source string
+     * @param shift        shift value
+     * @return encrypted string
+     */
+    @Override
+    public String encrypt(String sourceString, int shift) {
+        return null;
+    }
+
+    /**
+     * Decrypt protected string by unshifting it by <code>shift</code> value.
+     *
+     * @param encryptedString encrypted string
+     * @param shift           shift value
+     * @return decrypted string
+     */
+    @Override
+    public String decrypt(String encryptedString, int shift) {
+        return null;
+    }
+}
