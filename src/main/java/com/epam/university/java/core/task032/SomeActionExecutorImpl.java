1b1dde616eefb55aa8b815ec1b0a81e803275270
Romin <romin.nuro@gmail.com>
2020-09-11 22:42:25 +0300

    Task032 solved

---
+++ src/main/java/com/epam/university/java/core/task032/SomeActionExecutorImpl.java
@@ -0,0 +1,22 @@
+package com.epam.university.java.core.task032;
+
+/**
+ * Created by Romin Nuro on 11.09.2020 22:17.
+ */
+public class SomeActionExecutorImpl implements SomeActionExecutor {
+    /**
+     * Do the action, not important for us.
+     */
+    @Override
+    public void doAction() {
+        System.out.println("Doing some action");
+    }
+
+    /**
+     * Do another action, not important for us.
+     */
+    @Override
+    public void doAnotherAction() {
+        System.out.println("Doing another action");
+    }
+}
