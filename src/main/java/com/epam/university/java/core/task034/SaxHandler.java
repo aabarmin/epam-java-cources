beaea3ec2c2b3b396dbec63c81cade226b41059b
Dmitry Novikov <novicovdm@gmail.com>
2020-09-16 22:14:29 +0300

    Task 34 solved

--- src/main/java/com/epam/university/java/core/task034/SaxHandler.java
+++ src/main/java/com/epam/university/java/core/task034/SaxHandler.java
@@ -1,9 +1,13 @@
 package com.epam.university.java.core.task034;
 
+import java.util.HashSet;
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
 
 /**
  * Sax handler base.
  */
 public abstract class SaxHandler extends DefaultHandler {
+
 }
