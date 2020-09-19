bbec0eeef67d8bc959f241041d19f94bb490e1f6
Anton Kim <a.a.kim3101@gmail.com>
2020-09-19 21:40:05 +0300

    Task023 is implemented.

---
+++ src/main/java/com/epam/university/java/core/task023/Task023Impl.java
@@ -0,0 +1,11 @@
+package com.epam.university.java.core.task023;
+
+public class Task023Impl implements Task023 {
+    @Override
+    public String extract(String phoneString) {
+        if (phoneString == null || phoneString.length() < 10) {
+            throw new IllegalArgumentException();
+        }
+        return phoneString.replaceAll("[+(|)/\\s|-]", "").substring(1, 4);
+    }
+}
