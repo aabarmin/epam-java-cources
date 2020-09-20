1f258c8ba6b3a497bedf59d3ffe1f3285a65a15a
Telepaev Petr <petrtelepaev@yandex.ru>
2020-09-20 13:54:26 +0300

    something strange with equal freq

---
+++ src/main/java/com/epam/university/java/core/task039/CodeTreeNode.java
@@ -0,0 +1,83 @@
+package com.epam.university.java.core.task039;
+
+/**
+ * Huffman tree node
+ */
+
+public class CodeTreeNode implements Comparable<CodeTreeNode> {
+
+    private Character content;
+    private int weight;
+    private CodeTreeNode left;
+    private CodeTreeNode right;
+
+
+    public CodeTreeNode(Character content, int weight) {
+        this.content = content;
+        this.weight = weight;
+    }
+
+    public CodeTreeNode(Character content, int weight, CodeTreeNode left, CodeTreeNode right) {
+        this.content = content;
+        this.weight = weight;
+        this.left = left;
+        this.right = right;
+    }
+
+    public Character getContent() {
+        return content;
+    }
+
+    public void setContent(Character content) {
+        this.content = content;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public CodeTreeNode getLeft() {
+        return left;
+    }
+
+    public void setLeft(CodeTreeNode left) {
+        this.left = left;
+    }
+
+    public CodeTreeNode getRight() {
+        return right;
+    }
+
+    public void setRight(CodeTreeNode right) {
+        this.right = right;
+    }
+
+    public String getCodeForCharacter(Character ch, String codePath) {
+        if (this.getContent() == ch) {
+            return codePath;
+        } else {
+            if (left != null) {
+                String path = left.getCodeForCharacter(ch, codePath + "0");
+                if (path != null) {
+                    return path;
+                }
+            }
+            if (right != null) {
+                String path = right.getCodeForCharacter(ch, codePath + "1");
+                if (path != null) {
+                    return path;
+                }
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public int compareTo(CodeTreeNode o) {
+        return o.getWeight() - this.getWeight();
+    }
+}
