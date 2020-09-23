c424a0381749eec0ae71764676cc4663349327ec
Aleksandr Barmin <barmin.alexander@gmail.com>
2020-09-23 09:17:29 +0300

    Fix style

--- src/test/java/com/epam/university/java/core/task040/Task040Test.java
+++ src/test/java/com/epam/university/java/core/task040/Task040Test.java
@@ -4,7 +4,7 @@ import com.epam.university.java.core.helper.TestHelper;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class Task040Test {
 
