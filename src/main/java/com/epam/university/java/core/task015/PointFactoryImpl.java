774a9ee1ebb2fb7905d1ea629427e6a51483d2dc
Dmitry Novikov <novicovdm@gmail.com>
2020-10-01 05:28:43 +0300

    Modified a bit

--- src/main/java/com/epam/university/java/core/task015/PointFactoryImpl.java
+++ src/main/java/com/epam/university/java/core/task015/PointFactoryImpl.java
@@ -3,7 +3,7 @@ package com.epam.university.java.core.task015;
 /**
  * Author Dmitry Novikov 28-Sep-20.
  */
-public class PointFactoryImpl implements PointFactory{
+public class PointFactoryImpl implements PointFactory {
     @Override
     public Point newInstance(double x, double y) {
         return new PointImpl(x,y);
