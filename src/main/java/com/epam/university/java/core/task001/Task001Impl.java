2f62b0315f80233d17246e171557ed914a786f7f
Dmitry Vorotnikov <dmassoo.tv@gmail.com>
2020-10-01 17:04:22 +0800

    Task001 is here.

---
+++ src/main/java/com/epam/university/java/core/task001/Task001Impl.java
@@ -0,0 +1,46 @@
+package com.epam.university.java.core.task001;
+
+public class Task001Impl implements Task001 {
+
+    @java.lang.Override
+    public double addition(String firstNumber, String secondNumber) throws IllegalArgumentException,
+                                                                                 NumberFormatException{
+        if (firstNumber == null || secondNumber == null) {
+            throw new IllegalArgumentException();
+        }
+
+        return (Double.parseDouble(firstNumber) + Double.parseDouble(secondNumber));
+    }
+
+    @java.lang.Override
+    public double subtraction(String firstNumber, String secondNumber) throws IllegalArgumentException,
+                                                                                NumberFormatException{
+        if (firstNumber == null || secondNumber == null) {
+            throw new IllegalArgumentException();
+        }
+
+        return (Double.parseDouble(firstNumber) - Double.parseDouble(secondNumber));
+    }
+
+    @java.lang.Override
+    public double multiplication(String firstNumber, String secondNumber) throws IllegalArgumentException,
+                                                                                    NumberFormatException {
+
+        if (firstNumber == null || secondNumber == null) {
+            throw new IllegalArgumentException();
+        }
+
+        return (Double.parseDouble(firstNumber) * Double.parseDouble(secondNumber));
+    }
+
+    @java.lang.Override
+    public double division(String firstNumber, String secondNumber) throws IllegalArgumentException,
+                                                                                NumberFormatException {
+
+        if (firstNumber == null || secondNumber == null) {
+            throw new IllegalArgumentException();
+        }
+
+        return (Double.parseDouble(firstNumber) / Double.parseDouble(secondNumber));
+    }
+}
