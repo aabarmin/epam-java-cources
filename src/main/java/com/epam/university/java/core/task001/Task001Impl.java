925406f132d72cb2dccbb037170a7dfed05542bd
Dmitry Vorotnikov <dmassoo.tv@gmail.com>
2020-10-01 20:48:23 +0800

    CheckstyleMain is ok now I hope...

--- src/main/java/com/epam/university/java/core/task001/Task001Impl.java
+++ src/main/java/com/epam/university/java/core/task001/Task001Impl.java
@@ -3,8 +3,8 @@ package com.epam.university.java.core.task001;
 public class Task001Impl implements Task001 {
 
     @java.lang.Override
-    public double addition(String firstNumber, String secondNumber) throws IllegalArgumentException,
-            NumberFormatException {
+    public double addition(String firstNumber, String secondNumber)
+            throws IllegalArgumentException, NumberFormatException {
         if (firstNumber == null || secondNumber == null) {
             throw new IllegalArgumentException();
         }
@@ -13,8 +13,8 @@ public class Task001Impl implements Task001 {
     }
 
     @java.lang.Override
-    public double subtraction(String firstNumber, String secondNumber) throws
-            IllegalArgumentException, NumberFormatException {
+    public double subtraction(String firstNumber, String secondNumber)
+            throws IllegalArgumentException, NumberFormatException {
         if (firstNumber == null || secondNumber == null) {
             throw new IllegalArgumentException();
         }
@@ -23,8 +23,8 @@ public class Task001Impl implements Task001 {
     }
 
     @java.lang.Override
-    public double multiplication(String firstNumber, String secondNumber) throws
-            IllegalArgumentException, NumberFormatException {
+    public double multiplication(String firstNumber, String secondNumber)
+            throws IllegalArgumentException, NumberFormatException {
 
         if (firstNumber == null || secondNumber == null) {
             throw new IllegalArgumentException();
@@ -34,8 +34,8 @@ public class Task001Impl implements Task001 {
     }
 
     @java.lang.Override
-    public double division(String firstNumber, String secondNumber) throws IllegalArgumentException,
-            NumberFormatException {
+    public double division(String firstNumber, String secondNumber)
+            throws IllegalArgumentException, NumberFormatException {
 
         if (firstNumber == null || secondNumber == null) {
             throw new IllegalArgumentException();
