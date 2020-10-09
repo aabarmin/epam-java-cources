e34702f70e1f14382a0df6d8fc958b37a58031d3
Aleksandr Barmin <barmin.alexander@gmail.com>
2020-10-09 09:58:43 +0300

    Fixing style for Task045

--- src/main/java/com/epam/university/java/core/task045/Task045.java
+++ src/main/java/com/epam/university/java/core/task045/Task045.java
@@ -6,9 +6,11 @@ package com.epam.university.java.core.task045;
 public interface Task045 {
 
     /***
+     * <p>
      * Write a method that reverses all the words of input text.
      * All non-letter symbols should stay on the same places.
      * Use Latin alphabet for test only.
+     * </p>
      *
      * @param input string to convert
      * @return String - the result of the anagram
