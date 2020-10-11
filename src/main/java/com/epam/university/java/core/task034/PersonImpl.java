536a4acb0c1f272f61330e09005a1f7ca32cbdeb
Anton Kim <a.a.kim3101@gmail.com>
2020-10-12 00:44:13 +0300

    Task035 is implemented

--- src/main/java/com/epam/university/java/core/task034/PersonImpl.java
+++ src/main/java/com/epam/university/java/core/task034/PersonImpl.java
@@ -1,7 +1,8 @@
 package com.epam.university.java.core.task034;
 
-import com.google.gson.annotations.SerializedName;
-
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonRootName;
+import com.fasterxml.jackson.annotation.JsonSetter;
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlElementWrapper;
@@ -10,14 +11,16 @@ import javax.xml.bind.annotation.XmlRootElement;
 import java.util.Collection;
 import java.util.LinkedList;
 
+@JsonRootName(value = "person")
+@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
 @XmlRootElement(name = "person")
 //@XmlType(propOrder = {"id", "firstName", "lastName", "phoneNumbers"})
 public class PersonImpl implements Person {
-
     private int id;
     private String firstName;
     private String lastName;
     Collection<PhoneNumber> phoneNumbers = new LinkedList<>();
+
 
     @Override
     public int getId() {
@@ -30,6 +33,7 @@ public class PersonImpl implements Person {
         this.id = id;
     }
 
+
     @Override
     public String getFirstName() {
         return this.firstName;
@@ -40,6 +44,7 @@ public class PersonImpl implements Person {
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
+
 
     @Override
     public String getLastName() {
@@ -52,11 +57,13 @@ public class PersonImpl implements Person {
         this.lastName = lastName;
     }
 
+
     @Override
     public Collection<PhoneNumber> getPhoneNumbers() {
         return this.phoneNumbers;
     }
 
+    @JsonSetter(value = "phones")
     @XmlElementWrapper(name = "person-phones")
     @XmlElements({@XmlElement(type = PhoneNumberImpl.class, name = "person-phone")})
     @Override
--- src/main/java/com/epam/university/java/core/task034/PhoneNumberImpl.java
+++ src/main/java/com/epam/university/java/core/task034/PhoneNumberImpl.java
@@ -1,9 +1,19 @@
 package com.epam.university.java.core.task034;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import javax.xml.bind.annotation.XmlValue;
 
+@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
 public class PhoneNumberImpl implements PhoneNumber {
     private String phoneNumber;
+
+    public PhoneNumberImpl() {
+    }
+
+    public PhoneNumberImpl(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
 
     @Override
     public String getPhoneNumber() {
