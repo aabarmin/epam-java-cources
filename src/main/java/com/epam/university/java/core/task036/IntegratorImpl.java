84527cef084d03b6314d3670e062b485aeb6cb17
Petr Telepaev <Petrtelepaev@yandex.com>
2020-10-23 23:25:17 +0300

    Task036 is done

---
+++ src/main/java/com/epam/university/java/core/task036/IntegratorImpl.java
@@ -0,0 +1,32 @@
+package com.epam.university.java.core.task036;
+
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+import java.util.function.Function;
+
+public class IntegratorImpl implements Integrator {
+
+    public static final double INCREMENT = 1E-6;
+
+    @Override
+    public double integrate(double left, double right, Function<Double, Double> function) {
+        double area = 0;
+        double modifier = 1;
+//        if (left > right) {
+//            double tmp = left;
+//            left = right;
+//            right = tmp;
+//            modifier = -1;
+//        }
+        for (double i = left + INCREMENT; i < right; i += INCREMENT) {
+            double dFromLeft = i - left;
+            area += (INCREMENT / 2) * function.apply(left + dFromLeft) + function.apply(left + dFromLeft - INCREMENT);
+        }
+        area /= 10;
+        area = Math.round(area);
+
+
+        int result = (int) area;
+        return Math.round(result )/ 100000.0 * modifier;
+    }
+}
