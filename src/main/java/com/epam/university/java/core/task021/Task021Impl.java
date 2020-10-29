bc5c3b903017edf549827da341d95a2784870c5e
Telepaev Petr <petrtelepaev@yandex.ru>
2020-10-29 21:53:16 +0300

    Task021 to PC

--- src/main/java/com/epam/university/java/core/task021/Task021Impl.java
+++ src/main/java/com/epam/university/java/core/task021/Task021Impl.java
@@ -1,5 +1,6 @@
 package com.epam.university.java.core.task021;
 
+import com.epam.university.java.core.task013.Vertex;
 import com.epam.university.java.core.task015.Point;
 
 import java.util.ArrayList;
@@ -15,13 +16,48 @@ public class Task021Impl implements Task021 {
 
         ArrayList<Point> listOfMinePositions = new ArrayList<>(minePositions);
 
-        Point A = listOfMinePositions.get(0);
-        Point B = listOfMinePositions.get(1);
-        Point C = listOfMinePositions.get(2);
+//      Get triangle points
+        Point vertexA = listOfMinePositions.get(0);
+        Point vertexB = listOfMinePositions.get(1);
+        Point vertexC = listOfMinePositions.get(2);
 
+//      Get angles
 
+        double angleA = findAngle(vertexC, vertexA, vertexB);
+        double angleB = findAngle(vertexA, vertexB, vertexC);
+        double angleC = findAngle(vertexA, vertexC, vertexB);
 
 
         return null;
+
+    }
+
+    private double findAngle(Point vertexC, Point vertexA, Point vertexB) {
+        double angle = 0.0;
+
+
+        double modifiedXOfB;
+        double modifiedYOfB;
+        double modifiedXOfC;
+        double modifiedYOfC;
+
+        modifiedXOfB = vertexB.getX() - vertexA.getX();
+        modifiedYOfB = vertexB.getY() - vertexA.getY();
+
+        modifiedXOfC = vertexC.getX() - vertexA.getX();
+        modifiedYOfC = vertexC.getY() - vertexA.getY();
+
+        double scMul = modifiedXOfB * modifiedXOfC
+                + modifiedYOfB * modifiedYOfC;
+        double moduloOfB = Math.sqrt(Math.pow(modifiedXOfB, 2)
+                + Math.pow(modifiedYOfB, 2));
+        double moduloOfC = Math.sqrt(Math.pow(modifiedXOfC, 2)
+                + Math.pow(modifiedYOfC, 2));
+        double cosOfAngle = scMul / (moduloOfB * moduloOfC);
+
+        angle = Math.acos(cosOfAngle);
+
+        angle = Math.toDegrees(angle);
+        return angle;
     }
 }
