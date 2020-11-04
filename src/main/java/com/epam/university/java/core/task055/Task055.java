1045f4666dfd8f97bee5afd48cf0cd48e2f49cc7
ZzOneDay <azminseo@gmail.com>
2020-11-04 17:37:08 +0300

    for checkStyle

--- src/main/java/com/epam/university/java/core/task055/Task055.java
+++ src/main/java/com/epam/university/java/core/task055/Task055.java
@@ -3,30 +3,33 @@ package com.epam.university.java.core.task055;
 /**
  * Deliveryman with QuadCopter.
  * Imagine, You are delivery man with QuaCopter.
- * You works in company and deliver little posts,
+ * You work in company and deliver little posts,
  * but, people don't say number of window and number of floor.
  * You don't want to count of window every time. You are future programmer.
  * You decided create program that find params of need window.
- *
+ * <p>
  * Every floor are identical. Every flat identical.
  * Plan of floor with numbers of flat (exp. from number 1):
  * 6 : 5 : 4 : 3
  * 7 : 8 | 1 : 2
- *
+ * </p>
+ * <p>
  * "|" - is entrance in floor.
- *
+ * </p>
+ * <p>
  * Every flat has 2 windows.
  * If you enter in flat, left room is living room, right room is kitchen.
  * You deliver post to living room every.
- *
+ * </p>
  */
 public interface Task055 {
 
     /**
      * Every house has levels, and entrances.
      * This method returns correct window for delivery.
-     * @param level - How many levels are there in the house?
-     * @param entrances - How many entrances are there in the house?
+     *
+     * @param level        - How many levels are there in the house?
+     * @param entrances    - How many entrances are there in the house?
      * @param numberOfFlat - number of flat from task of delivery.
      */
     public Window getWindowForDelivery(int level, int entrances, int numberOfFlat);
