4c9a112016f559fc5e111db3dc99ecca5f40e65c
egorgorchak <36458220+egorgorchak@users.noreply.github.com>
2020-11-05 15:22:56 +0300

    checkstyle fix

--- src/main/java/com/epam/university/java/core/task056/Task056.java
+++ src/main/java/com/epam/university/java/core/task056/Task056.java
@@ -4,37 +4,40 @@ import java.util.Collection;
 
 /**
  * Treatment.
- * 
- * Boris felt sick. He went to the hospital for treatment. At the hospital the doctor prescribed
- * Boris to take n-packs of pills and designated a certain period of days to take each pack. During
- * treatment the following conditions must be observed:
- * 1. Boris can only take one pill a day.
- * 2. Pills from n-pack should be taken only during the period of days prescribed by the doctor.
- * 3. Boris cannot start a new pack if the last one isn't finished.
- * 4. It's forbidden to take pills from different packs at the same time.
  *
- * Boris needs to choose a combination of packs to maximize the number of pills.
+ * <p>
+ *     Boris felt sick. He went to the hospital for treatment. At the hospital the doctor prescribed
+ *     Boris to take n-packs of pills and designated a certain period of days to take each pack.
+ *     During treatment the following conditions must be observed:
+ *     1. Boris can only take one pill a day.
+ *     2. Pills from n-pack should be taken only during the period of days prescribed by the doctor.
+ *     3. Boris cannot start a new pack if the last one isn't finished.
+ *     4. It's forbidden to take pills from different packs at the same time.
  *
- * In addition, it's prohibited to drink alcohol on the days of taking medication. Boris quite upset
- * about this fact.
+ *     Boris needs to choose a combination of packs to maximize the number of pills.
  *
- * If the number of packs is more than one, then Boris need to determine the interval between taking
- * medication, if such exists.
+ *     In addition, it's prohibited to drink alcohol on the days of taking medication. Boris quite
+ *     upset about this fact.
  *
- * For example:
- *      The doctor prescribed 4 packs of pills. Each line contains the number of pills in the pack
- *      and period for taking medication (separated by space).
+ *     If the number of packs is more than one, then Boris need to determine the interval between
+ *     taking medication, if such exists.
  *
- *      Prescription:
- *      3 1-3
- *      10 3-12
- *      5 6-10
- *      3 12-14
+ *          For example:
+ *          The doctor prescribed 4 packs of pills. Each line contains the number of pills in the
+ *          pack and period for taking medication (separated by space).
  *
- *      Packs: [0, 2, 3] (the number of pills in this combination is maximum)
- *      Interval between medication: [4-5, 11-11] (these days Boris can treat himself with apple
- *      cider)
+ *          Prescription:
+ *          3 1-3
+ *          10 3-12
+ *          5 6-10
+ *          3 12-14
+ *
+ *          Packs: [0, 2, 3] (the number of pills in this combination is maximum)
+ *          Interval between medication: [4-5, 11-11] (these days Boris can treat himself with apple
+ *          cider)
+ * </p>
  */
+
 public interface Task056 {
     /**
      * Determination of the required packs of pills.
