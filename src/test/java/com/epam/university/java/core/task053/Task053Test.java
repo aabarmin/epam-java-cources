802a39abb54df5bf14ea5c2ca11eabc08ace0365
Vladimir Baykalov <Baikalov.v42@yandex.ru>
2020-11-06 15:46:32 +0300

    new test for 053 - bracers order

--- src/test/java/com/epam/university/java/core/task053/Task053Test.java
+++ src/test/java/com/epam/university/java/core/task053/Task053Test.java
@@ -36,6 +36,21 @@ public class Task053Test {
         instance.calculate("1+3-6+#");
     }
 
+    @Test(expected = IllegalArgumentException.class)
+    public void first_BracketOrderNotValid() throws Exception {
+        instance.calculate("(1+1)*((5-3)");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void second_BracketOrderNotValid() throws Exception {
+        instance.calculate("(1+1)*(5-3))");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void third_BracketOrderNotValid() throws Exception {
+        instance.calculate("3-1)");
+    }
+
     @Test
     public void first() {
         String input = "1+2+3";
