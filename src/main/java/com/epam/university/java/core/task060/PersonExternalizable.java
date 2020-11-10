a8b764b63d0510d9723608a03acae7937b0d4e45
egorgorchak <36458220+egorgorchak@users.noreply.github.com>
2020-11-10 16:59:09 +0300

    getters was deleted

--- src/main/java/com/epam/university/java/core/task060/PersonExternalizable.java
+++ src/main/java/com/epam/university/java/core/task060/PersonExternalizable.java
@@ -5,23 +5,13 @@ import java.util.Collection;
 
 public interface PersonExternalizable extends Externalizable {
 
-    String getFullName();
-
     void setFullName(String fullName);
-
-    int getAge();
 
     void setAge(int age);
 
-    boolean isMale();
-
     void setMale(boolean male);
 
-    PersonExternalizable getSpouse();
-
     void setSpouse(PersonExternalizable spouse);
-
-    Collection<PersonExternalizable> getChildren();
 
     void setChildren(Collection<PersonExternalizable> children);
 }
--- src/main/java/com/epam/university/java/core/task060/PersonSerializable.java
+++ src/main/java/com/epam/university/java/core/task060/PersonSerializable.java
@@ -5,23 +5,13 @@ import java.util.Collection;
 
 public interface PersonSerializable extends Serializable {
 
-    String getFullName();
-
     void setFullName(String fullName);
-
-    int getAge();
 
     void setAge(int age);
 
-    boolean isMale();
-
     void setMale(boolean male);
 
-    PersonSerializable getSpouse();
-
     void setSpouse(PersonSerializable spouse);
-
-    Collection<PersonSerializable> getChildren();
 
     void setChildren(Collection<PersonSerializable> children);
 }
