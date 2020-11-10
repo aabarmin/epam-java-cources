cd95ff2ea6f23e4791e19e04c9571c66c7d4498c
egorgorchak <36458220+egorgorchak@users.noreply.github.com>
2020-11-10 21:59:44 +0300

    checkstyle fix

--- src/test/java/com/epam/university/java/core/task063/Task063Test.java
+++ src/test/java/com/epam/university/java/core/task063/Task063Test.java
@@ -7,7 +7,9 @@ import org.junit.Test;
 import java.io.OutputStream;
 import java.util.List;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertSame;
 
 public class Task063Test {
     private Task063 instance;
