e5cf62f295ae97c8b2f673d96dc2447aaa35b9ec
Anton Kim <a.a.kim3101@gmail.com>
2020-11-11 22:50:10 +0300

    Task060 is added.

---
+++ src/main/java/com/epam/university/java/core/task060/Rank.java
@@ -0,0 +1,17 @@
+package com.epam.university.java.core.task062;
+
+public enum Rank {
+    TWO,
+    THREE,
+    FOUR,
+    FIVE,
+    SIX,
+    SEVEN,
+    EIGHT,
+    NINE,
+    TEN,
+    JACK,
+    QUEEN,
+    KING,
+    ACE
+}
---
+++ src/main/java/com/epam/university/java/core/task060/Suit.java
@@ -0,0 +1,8 @@
+package com.epam.university.java.core.task062;
+
+public enum Suit {
+    CLUB,
+    DIAMOND,
+    HEART,
+    SPADE
+}
