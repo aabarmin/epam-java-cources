caae5b219949ba4ee45ca4071ccb58f1284e95ad
egorgorchak <36458220+egorgorchak@users.noreply.github.com>
2020-11-16 11:29:47 +0300

    task 62 was fixed

--- src/main/java/com/epam/university/java/core/task062/SingletonObject.java
+++ src/main/java/com/epam/university/java/core/task062/SingletonObject.java
@@ -1,13 +1,4 @@
 package com.epam.university.java.core.task062;
 
 public interface SingletonObject {
-
-    /**
-     * Return or create (if not created yet) a singleton instance.
-     *
-     * @return singleton instance
-     */
-    static SingletonObject getInstance() {
-        return null;
-    }
 }
