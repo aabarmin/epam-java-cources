cf558f51fe413766084182a919668e3fffa4923b
absintminded <absinthminded666@gmail.com>
2020-11-17 17:59:20 +0300

    new task

---
+++ src/main/java/com/epam/university/java/core/task068/Task068.java
@@ -0,0 +1,15 @@
+package com.epam.university.java.core.task068;
+
+import java.util.List;
+
+/**
+ * In this task you should calculate ratios of elements that are positive, negative and zeroes.
+ */
+public interface Task068 {
+    /**
+     * Function to calculate the numbers.
+     * @param listOfNumbers list of input numbers;
+     * @return collection of ratios.
+     */
+    public List<Double> countOfRatios(List<Integer> listOfNumbers);
+}
