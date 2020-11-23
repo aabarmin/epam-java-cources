6860f98f14cfd7c963dd97cbf46267b56c5cbb40
Aleksandr Barmin <barmin.alexander@gmail.com>
2020-11-23 10:48:21 +0300

    Update and rename Task068.java to Task073.java

--- src/main/java/com/epam/university/java/core/task068/Task073.java
+++ src/main/java/com/epam/university/java/core/task068/Task073.java
@@ -5,7 +5,7 @@ import java.util.List;
 /**
  * In this task you should calculate ratios of elements that are positive, negative and zeroes.
  */
-public interface Task068 {
+public interface Task073 {
     /**
      * Function to calculate the numbers.
      * @param listOfNumbers list of input numbers;
